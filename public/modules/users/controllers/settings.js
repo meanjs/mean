7ba7dcfd2155773fb3c8bffe516547793a370d6f
Amos Haviv <mail@amoshaviv.com>
2014-03-26 01:30:58 +0200

    Adding New files

---
+++ public/modules/users/controllers/settings.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('mean.users').controller('SettingsController', ['$scope', '$http', '$location', 'Users', 'Authentication',
+	function($scope, $http, $location, Users, Authentication) {
+		$scope.user = Authentication.user;
+
+		//If user is not signed in then redirect back home
+        if (!$scope.user) $location.path('/');
+
+		$scope.updateUserProfile = function() {
+			$scope.success = $scope.error = null;
+			var user = new Users($scope.user);
+
+			user.$update(function(response) {
+				$scope.success = true;
+				Authentication.user = response;
+			}, function(response) {
+				$scope.error = response.data.message;
+			});
+		};
+
+		$scope.changeUserPassword = function() {
+			$scope.success = $scope.error = null;
+
+			$http.post('/users/password', $scope.passwordDetails).success(function(response) {
+                // If successful show success message and clear form
+				$scope.success = true;
+                $scope.passwordDetails = null;
+            }).error(function(response) {
+                $scope.error = response.message;
+            });
+		};
+	}
+]);
---
+++ public/modules/users/services/users.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Users service used for users REST endpoint
+angular.module('mean.users').factory('Users', ['$resource',
+	function($resource) {
+		return $resource('users', {}, {
+			update: {
+				method: 'PUT'
+			}
+		});
+	}
+]);
