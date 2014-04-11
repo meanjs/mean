c9d641e2bcddce80492943269e449c4f8e4737cd
Marc-André Bélanger <bitcoin.marc.andre.belanger@gmail.com>
2014-04-10 22:26:41 -0400

    Migration to Express 4.x
    Please see the Express documentation for details.
    
    connect-mongo NPM package is currently broken with Express 4.x. Use
    this fix in the meantime:
    https://github.com/kcbanner/connect-mongo/issues/103#issuecomment-400800
    50 .

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -7,11 +7,14 @@ var users = require('../../app/controllers/users'),
 	articles = require('../../app/controllers/articles');
 
 module.exports = function(app) {
-	// Article Routes
-	app.get('/articles', articles.list);
-	app.post('/articles', users.requiresLogin, articles.create);
-	app.get('/articles/:articleId', articles.read);
-	app.put('/articles/:articleId', users.requiresLogin, articles.hasAuthorization, articles.update);
+	// Article Routes, using express 4.x syntax
+	app.route('/articles')
+		.get(articles.list)
+		.post(users.requiresLogin, articles.create);
+	
+	app.route('/articles/:articleId')
+		.get(articles.read)
+		.put(users.requiresLogin, articles.hasAuthorization, articles.update);
 	app.del('/articles/:articleId', users.requiresLogin, articles.hasAuthorization, articles.delete);
 
 	// Finish by binding the article middleware
