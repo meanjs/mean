14c3ce7cc995a3267b12e86a7a1f0e8bf33be140
Amos Haviv <mail@amoshaviv.com>
2014-04-28 22:04:44 +0300

    Fix Config

--- public/config.js
+++ public/config.js
@@ -1,16 +1,23 @@
 'use strict';
 
-// Init the application configuration object for AngularJS application
+// Init the application configuration module for AngularJS application
 var ApplicationConfiguration = (function() {
-	return {
-		applicationModuleName: 'mean',
-		applicationModuleVendorDependencies: ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.utils'],
-		registerModule: function(moduleName) {
-			// Create angular module
-			angular.module(moduleName, []);
+	// Init module configuration options
+	var applicationModuleName = 'mean';
+	var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.utils'];
 
-			// Add the module to the AngularJS configuration file
-			angular.module(this.applicationModuleName).requires.push(moduleName);
-		}
+	// Add a new vertical module
+	var registerModule = function(moduleName) {
+		// Create angular module
+		angular.module(moduleName, []);
+
+		// Add the module to the AngularJS configuration file
+		angular.module(applicationModuleName).requires.push(moduleName);
+	};
+
+	return {
+		applicationModuleName: applicationModuleName,
+		applicationModuleVendorDependencies: applicationModuleVendorDependencies,
+		registerModule: registerModule
 	};
 })();
