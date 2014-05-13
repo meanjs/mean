46092dd1d26ff1f6cd57db8d9b28ba2b100fff18
Michael Cole <michaelcole@michaelcole.com>
2014-05-12 22:24:14 -0500

    Reformat and bootstrappify the client home view.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,5 +1,8 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', function ($scope, Authentication) {
+angular.module('core')
+  .controller('HomeController', ['$scope', 'Authentication', function ($scope, Authentication) {
+    // This provides Authentication context.
+    // see /public/modules/users/services/authentication.client.service.js
     $scope.authentication = Authentication;
-}]);
+  }]);
