24ceb7d6abd01744934adf9b1a517d71433d8a9c
Anastasis Andronidis <anastasis90@yahoo.gr>
2014-05-15 16:12:40 +0200

    Fixing module deps in angular

--- public/modules/core/core.client.module.js
+++ public/modules/core/core.client.module.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Use Applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('core');
+ApplicationConfiguration.registerModule('core', []);
