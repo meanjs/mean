2b655c0d88318181aae59819b59676d4eb2f860f
Amos Haviv <mail@amoshaviv.com>
2014-05-15 18:13:13 +0300

    Fix vertical issues

--- server.js
+++ server.js
@@ -3,9 +3,14 @@
  * Module dependencies.
  */
 var MEAN = require('mean-core');
+var admin = require('mean-admin');
 
 // Init App
 var application = new MEAN();
+var expressApplication = application.listen();
+
+// Attach admin
+admin(expressApplication);
 
 // Expose App
-exports = module.exports = application.listen();
+exports = module.exports = expressApplication;
