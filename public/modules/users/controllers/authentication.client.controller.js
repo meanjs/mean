725cc4ce333783e3e9ad6ca67fc43c44185d2d8f
Keith <keith@keithfimreite.com>
2014-06-05 22:31:32 -0700

    Client-side Validation updates

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -7,16 +7,20 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		//If user is signed in then redirect back home
 		if ($scope.authentication.user) $location.path('/');
 
-		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
-				//If successful we assign the response to the global user model
-				$scope.authentication.user = response;
-
-				//And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
+		$scope.signup = function(isValid) {
+			if (isValid){
+				$http.post('/auth/signup', $scope.credentials).success(function(response) {
+					//If successful we assign the response to the global user model
+					$scope.authentication.user = response;
+	
+					//And redirect to the index page
+					$location.path('/');
+				}).error(function(response) {
+					$scope.error = response.message;
+				});
+			} else {
+				$scope.submitted = true;
+			}
 		};
 
 		$scope.signin = function() {
