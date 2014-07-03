83686b82d9a126374d3a36f3bb97a511bc11ef85
Matthias Luebken <matthias.luebken@gmail.com>
2014-07-03 10:04:11 +0200

    - reading the right port in production - changed default back to development

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var DB_HOST = process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost';
+
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/mean',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + DB_HOST + '/mean',
 	assets: {
 		lib: {
 			css: [
