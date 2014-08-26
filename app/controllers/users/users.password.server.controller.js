ab821ecc3caead334fc09e949e21c305520135c4
Yarin Podoler <yarin.podler@startapp.com>
2014-08-26 12:11:21 -0400

    Added: Separation of user password controller to user password service with forgot password method

--- app/controllers/users/users.password.server.controller.js
+++ app/controllers/users/users.password.server.controller.js
@@ -12,80 +12,21 @@ var _ = require('lodash'),
 	nodemailer = require('nodemailer'),
 	crypto = require('crypto'),
 	async = require('async'),
-	crypto = require('crypto');
+    userPassService = require('../../services/users.password.server.service');
 
 /**
  * Forgot for reset password (forgot POST)
  */
 exports.forgot = function(req, res, next) {
-	async.waterfall([
-		// Generate random token
-		function(done) {
-			crypto.randomBytes(20, function(err, buffer) {
-				var token = buffer.toString('hex');
-				done(err, token);
-			});
-		},
-		// Lookup user by username
-		function(token, done) {
-			if (req.body.username) {
-				User.findOne({
-					username: req.body.username
-				}, '-salt -password', function(err, user) {
-					if (!user) {
-						return res.status(400).send({
-							message: 'No account with that username has been found'
-						});
-					} else if (user.provider !== 'local') {
-						return res.status(400).send({
-							message: 'It seems like you signed up using your ' + user.provider + ' account'
-						});
-					} else {
-						user.resetPasswordToken = token;
-						user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+    userPassService.forgotPassword(req.body.username, req.headers.host, function(err, email){
+        if (err){
+            return next(err);
+        }
 
-						user.save(function(err) {
-							done(err, token, user);
-						});
-					}
-				});
-			} else {
-				return res.status(400).send({
-					message: 'Username field must not be blank'
-				});
-			}
-		},
-		function(token, user, done) {
-			res.render('templates/reset-password-email', {
-				name: user.displayName,
-				appName: config.app.title,
-				url: 'http://' + req.headers.host + '/auth/reset/' + token
-			}, function(err, emailHTML) {
-				done(err, emailHTML, user);
-			});
-		},
-		// If valid email, send reset email using service
-		function(emailHTML, user, done) {
-			var smtpTransport = nodemailer.createTransport(config.mailer.options);
-			var mailOptions = {
-				to: user.email,
-				from: config.mailer.from,
-				subject: 'Password Reset',
-				html: emailHTML
-			};
-			smtpTransport.sendMail(mailOptions, function(err) {
-				if (!err) {
-					res.send({
-						message: 'An email has been sent to ' + user.email + ' with further instructions.'
-					});
-				}
-
-				done(err);
-			});
-		}
-	], function(err) {
-		if (err) return next(err);
-	});
+        res.send({
+            message: 'An email has been sent to ' + email + ' with further instructions.'
+        });
+    });
 };
 
 /**
---
+++ app/services/users.password.server.service.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var async = require('async'),
+    mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    emailService = require('./email.server.service'),
+    config = require('../../config/config'),
+    utilsService = require('./utils.server.service');
+
+/**
+ * Forgot Password Service:
+ *  1. find the user
+ *  2. generate new token
+ * @param username - {String}
+ * @param callback - {Function} - in the form of function(err, email);
+ */
+exports.forgotPassword = function(username, hostname, callback){
+    async.waterfall([
+        // Search for the user
+        function(done) {
+            User.findByUsername(username, function(err, user){
+                if (err){
+                    return done(err);
+                }
+
+                if (user === null){
+                    return done (new Error('No user found'));
+                }
+
+                done(null, user);
+            });
+        },
+        // Generate a new token
+        function(user, done){
+            utilsService.generateToken(function(err, token){
+                done(err, user, token);
+            });
+        },
+        // Save new token
+        function(user, token, done){
+            user.resetPasswordToken = token;
+            user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+            user.save(function(err) {
+                done(err, user, token);
+            });
+        },
+        // send email
+        function(user, token, done){
+            var options = {
+                template : {
+                    path : 'app/views/templates/reset-password-email.server.view.html',
+                    renderOptions : {
+                        name: user.displayName,
+                        appName: config.app.title,
+                        url: 'http://' + hostname + '/auth/reset/' + token
+                    }
+                },
+                email : {
+                    to: user.email,
+                    subject: 'Password Reset',
+                }
+            };
+            emailService.sendTemplate(options, function(err){
+                done(err, user.email);
+            });
+        }
+    ], function(err, email){
+        callback(err, email);
+    });
+};
