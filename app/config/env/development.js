ebc17013a4b23140a71a65248a338807fb08f491
Layton Whiteley <layton.whiteley@gmail.com>
2014-09-03 14:13:07 -0400

    disable logging to console in prod by default - only allow server to push logs

--- app/config/env/development.js
+++ app/config/env/development.js
@@ -13,7 +13,7 @@ module.exports = {
 		config: {
 			replaceConsole: true,
 			appenders: [
-				{ type: 'console' },
+				{ type: 'console'},
 				{ type: 'file', filename: logOutputFile }
 			]
 		}
--- app/config/env/production.js
+++ app/config/env/production.js
@@ -10,7 +10,7 @@ module.exports = {
 		config: {
 			replaceConsole: true,
 			appenders: [
-				//{ type: 'console' },
+				{ type: 'console', category: 'server'},
 				{ type: 'file', filename: logOutputFile }
 			]
 		}
