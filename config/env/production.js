ea04c9e55afb216e575712f453cdff6649f51a27
Amos Haviv <mail@amoshaviv.com>
2014-10-07 17:29:06 +0300

    Small Update

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/mean',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
 	assets: {
 		lib: {
 			css: [
