d2d4e8c8659f3b90b0ebf0da0bad262792c0c21e
Liran Tal <liran.tal@gmail.com>
2014-11-06 13:17:17 +0200

    realized I forgot to add the actual logger

---
+++ config/logger.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var morgan = require('morgan');
+var config = require('./config');
+var fs = require('fs');
+
+/**
+ * Module init function.
+ */
+module.exports = {
+
+	getLogFormat: function() {
+		return config.log.format;
+	},
+
+	getLogOptions: function() {
+		var options = {};
+
+		if ('stream' in config.log.options) {
+			options = {
+				stream: fs.createWriteStream(process.cwd() + '/' + config.log.options.stream, {flags: 'a'})
+			};
+		}
+
+		return options;
+	}
+
+};
