6ec9233d1c5afd44790bf872e9f572ca18d62c07
Liran Tal <liran.tal@gmail.com>
2014-11-27 19:03:45 +0200

    updating script to attempt generating the ssl certificates directory first before trying to create files there

--- generate-ssl-certs.sh
+++ generate-ssl-certs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Generating self-signed certificates..."
+mkdir -p ./config/sslcerts
 openssl genrsa -out ./config/sslcerts/key.pem -aes256 1024
 openssl req -new -key ./config/sslcerts/key.pem -out ./config/sslcerts/csr.pem
 openssl x509 -req -days 9999 -in ./config/sslcerts/csr.pem -signkey ./config/sslcerts/key.pem -out ./config/sslcerts/cert.pem
