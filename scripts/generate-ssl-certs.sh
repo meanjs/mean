586f0ade38adb8c739a7e9fc830af2e393b1a91c
Liran Tal <liran.tal@gmail.com>
2014-11-28 09:29:18 +0200

    adding the new scripts directory

---
+++ scripts/generate-ssl-certs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Generating self-signed certificates..."
+mkdir -p ./config/sslcerts
+openssl genrsa -out ./config/sslcerts/key.pem -aes256 1024
+openssl req -new -key ./config/sslcerts/key.pem -out ./config/sslcerts/csr.pem
+openssl x509 -req -days 9999 -in ./config/sslcerts/csr.pem -signkey ./config/sslcerts/key.pem -out ./config/sslcerts/cert.pem
+rm ./config/sslcerts/csr.pem
+chmod 600 ./config/sslcerts/key.pem ./config/sslcerts/cert.pem
