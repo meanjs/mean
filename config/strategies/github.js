ea50c8b250e276f79a0128a2a6e5a61b489ddab5
Liran Tal <liran.tal@gmail.com>
2014-12-04 14:49:19 +0200

    fixed #286 which originated due to commit https://github.com/meanjs/mean/commit/fa1171fe280496fa97a6a8eed1027776922e5c65 which was then re-factored to leave out the url library

--- config/strategies/github.js
+++ config/strategies/github.js
@@ -4,7 +4,6 @@
  * Module dependencies.
  */
 var passport = require('passport'),
-	url = require('url'),
 	GithubStrategy = require('passport-github').Strategy,
 	config = require('../config'),
 	users = require('../../app/controllers/users.server.controller');
--- config/strategies/linkedin.js
+++ config/strategies/linkedin.js
@@ -4,7 +4,6 @@
  * Module dependencies.
  */
 var passport = require('passport'),
-	url = require('url'),
 	LinkedInStrategy = require('passport-linkedin').Strategy,
 	config = require('../config'),
 	users = require('../../app/controllers/users.server.controller');
