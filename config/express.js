f06460c8ca287aff2e2022493e90f401e91deaf2
Benjamin Montgomery <bmontgom@montynet.org>
2014-12-22 20:22:02 -0500

    move static files middleware before session

--- config/express.js
+++ config/express.js
@@ -76,6 +76,9 @@ module.exports = function(db) {
 		app.locals.cache = 'memory';
 	}
 
+	// Setting the app router and static folder
+	app.use(express.static(path.resolve('./public')));
+
 	// Request body parsing middleware should be above methodOverride
 	app.use(bodyParser.urlencoded({
 		extended: true
@@ -112,9 +115,6 @@ module.exports = function(db) {
 	app.use(helmet.nosniff());
 	app.use(helmet.ienoopen());
 	app.disable('x-powered-by');
-
-	// Setting the app router and static folder
-	app.use(express.static(path.resolve('./public')));
 
 	// Globbing routing files
 	config.getGlobbedFiles('./app/routes/**/*.js').forEach(function(routePath) {
