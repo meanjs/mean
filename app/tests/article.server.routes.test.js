995213881d683f0b940c000b3c7545019c020f07
Liran Tal <liran.tal@gmail.com>
2015-01-13 16:55:01 +0200

    adding test to check proper handling when no article exists

--- app/tests/article.server.routes.test.js
+++ app/tests/article.server.routes.test.js
@@ -201,6 +201,17 @@ describe('Article CRUD tests', function() {
 		});
 	});
 
+	it('should return proper error for single article which doesnt exist, if not signed in', function(done) {
+		request(app).get('/articles/test')
+			.end(function(req, res) {
+				// Set assertion
+				res.body.should.be.an.Object.with.property('message', 'Article is invalid');
+
+				// Call the assertion callback
+				done();
+			});
+	});
+
 	it('should be able to delete an article if signed in', function(done) {
 		agent.post('/auth/signin')
 			.send(credentials)
