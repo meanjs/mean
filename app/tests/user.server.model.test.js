56c288e92d08ef3ec800933b070e3ec642d1139c
Veikko Karsikko <veikko88@gmail.com>
2015-02-18 14:39:56 +0200

    Wait for async saving and removing

--- app/tests/user.server.model.test.js
+++ app/tests/user.server.model.test.js
@@ -52,11 +52,12 @@ describe('User Model Unit Tests:', function() {
 		});
 
 		it('should fail to save an existing user again', function(done) {
-			user.save();
-			return user2.save(function(err) {
-				should.exist(err);
-				done();
-			});
+			user.save(function(){
+        user2.save(function (err){
+          should.exist(err);
+          done();
+        });
+      });
 		});
 
 		it('should be able to show an error when try to save without first name', function(done) {
@@ -69,7 +70,8 @@ describe('User Model Unit Tests:', function() {
 	});
 
 	after(function(done) {
-		User.remove().exec();
-		done();
+		User.remove().exec(function(){
+			done();	
+		});
 	});
 });
