ad198aff1a2fe933b2db7f9a48020b015e0dbfe9
Cody B. Daig <cody@daig.me>
2015-02-26 09:55:02 -0700

    Updated getGlobbedFiles to work with glob 4.4.0

--- config/config.js
+++ config/config.js
@@ -18,38 +18,17 @@ module.exports = _.extend(
  * Get files by glob patterns
  */
 module.exports.getGlobbedFiles = function(globPatterns, removeRoot) {
-	// For context switching
-	var _this = this;
-
-	// URL paths regex
-	var urlRegex = new RegExp('^(?:[a-z]+:)?\/\/', 'i');
-
 	// The output array
 	var output = [];
 
-	// If glob pattern is array so we use each pattern in a recursive way, otherwise we use glob 
-	if (_.isArray(globPatterns)) {
-		globPatterns.forEach(function(globPattern) {
-			output = _.union(output, _this.getGlobbedFiles(globPattern, removeRoot));
-		});
-	} else if (_.isString(globPatterns)) {
-		if (urlRegex.test(globPatterns)) {
-			output.push(globPatterns);
-		} else {
-			glob(globPatterns, {
-				sync: true
-			}, function(err, files) {
-				if (removeRoot) {
-					files = files.map(function(file) {
-						return file.replace(removeRoot, '');
-					});
-				}
-
-				output = _.union(output, files);
-			});
-		}
+	var files = glob.sync(globPatterns);
+	if (removeRoot) {
+	    files = files.map(function(file) {
+	        return file.replace(removeRoot, '');
+	    });
 	}
-
+	
+	output = _.union(output, files);
 	return output;
 };
 
