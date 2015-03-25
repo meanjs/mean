1f290e425944e91d7f1c4f58c5b7491beb693e22
Calvin Moore <c1moore@ufl.edu>
2015-03-25 11:13:54 -0400

    Fix authentication problem
    Set window.user to null so one can not simply refresh the page and magically log back in without providing credentials.

--- public/modules/users/config/users.client.config.js
+++ public/modules/users/config/users.client.config.js
@@ -4,14 +4,15 @@
 angular.module('users').config(['$httpProvider',
 	function($httpProvider) {
 		// Set the httpProvider "not authorized" interceptor
-		$httpProvider.interceptors.push(['$q', '$location', 'Authentication',
-			function($q, $location, Authentication) {
+		$httpProvider.interceptors.push(['$q', '$location', 'Authentication', '$window',
+			function($q, $location, Authentication, $window) {
 				return {
 					responseError: function(rejection) {
 						switch (rejection.status) {
 							case 401:
 								// Deauthenticate the global user
 								Authentication.user = null;
+								$window.user = null;
 
 								// Redirect to signin page
 								$location.path('signin');
