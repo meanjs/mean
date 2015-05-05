d4de80979a5123c76a64b86dbed52bb886aa2b88
Andrew Throener <athroener@gmail.com>
2015-05-05 11:19:56 -0500

    Cleaned up console.logs in strategy.  Added to settings social accounts

--- config/strategies/paypal.js
+++ config/strategies/paypal.js
@@ -7,41 +7,8 @@ var passport = require('passport'),
 		PayPalStrategy = require('passport-paypal-openidconnect').Strategy,
 		config = require('../config'),
 		users = require('../../app/controllers/users.server.controller');
-/*
-module.exports = function() {
-	// Use google strategy
-	passport.use(new PayPalStrategy({
-				clientID: config.paypal.clientID,
-				clientSecret: config.paypal.clientSecret,
-				callbackURL: config.paypal.callbackURL,
-				passReqToCallback: true
-			},
-			function(req, accessToken, refreshToken, profile, done) {
-				// Set the provider data and include tokens
-				var providerData = profile._json;
-				providerData.accessToken = accessToken;
-				providerData.refreshToken = refreshToken;
 
-				// Create the user OAuth profile
-				var providerUserProfile = {
-					firstName: profile.name.givenName,
-					lastName: profile.name.familyName,
-					displayName: profile.displayName,
-					email: profile.emails[0].value,
-					username: profile.username,
-					provider: 'google',
-					providerIdentifierField: 'id',
-					providerData: providerData
-				};
-
-				// Save the user OAuth profile
-				users.saveOAuthUserProfile(req, providerUserProfile, done);
-			}
-	));
-};
-*/
 module.exports = function () {
-	console.log(config);
 	passport.use(new PayPalStrategy({
 			clientID: config.paypal.clientID,
 			clientSecret: config.paypal.clientSecret,
@@ -68,7 +35,6 @@ module.exports = function () {
 				providerIdentifierField: 'id',
 				providerData: providerData
 			};
-			console.log(providerUserProfile);
 
 			// Save the user OAuth profile
 			users.saveOAuthUserProfile(req, providerUserProfile, done);
