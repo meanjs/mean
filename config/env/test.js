3abbf429a23e50c440893cd9fe3a65f53ee12aac
Roberto Amador <robertoag89@gmail.com>
2015-05-05 16:28:30 +0000

    Fig changed to Compose
    Typo in environment
    
    Changed Compose href to https
    
    Wrong indentation
    
    Wrong indentation
    
    Wrong indentation
    
    New Line at the EOF
    
    Update docker-compose.yml

--- config/env/test.js
+++ config/env/test.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	db: {
-		uri: 'mongodb://localhost/mean-test',
+    		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-test',
 		options: {
 			user: '',
 			pass: ''
