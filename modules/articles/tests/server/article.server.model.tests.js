b1f6407303b1632b0b92cb2a5a5caeca30812d5d
MangeshMp <mangesh.pimpalkar@gmail.com>
2015-05-20 20:33:25 -0700

    reverted the code.

--- modules/articles/tests/server/article.server.model.tests.js
+++ modules/articles/tests/server/article.server.model.tests.js
@@ -1,16 +1,12 @@
 'use strict';
 
-var path = require('path');
-var User = require(path.resolve('./modules/users/server/models/user.server.model'));
-var Article = require(path.resolve('./modules/articles/server/models/article.server.model'));
-
 /**
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose');
-	User = mongoose.model('User');
-	Article = mongoose.model('Article');
+    mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    Article = mongoose.model('Article');
 
 /**
  * Globals
@@ -20,49 +16,49 @@ var user, article;
 /**
  * Unit tests
  */
-describe('Article Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
+describe('Article Model Unit Tests:', function () {
+    beforeEach(function (done) {
+        user = new User({
+            firstName: 'Full',
+            lastName: 'Name',
+            displayName: 'Full Name',
+            email: 'test@test.com',
+            username: 'username',
+            password: 'password'
+        });
 
-		user.save(function() {
-			article = new Article({
-				title: 'Article Title',
-				content: 'Article Content',
-				user: user
-			});
+        user.save(function () {
+            article = new Article({
+                title: 'Article Title',
+                content: 'Article Content',
+                user: user
+            });
 
-			done();
-		});
-	});
+            done();
+        });
+    });
 
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return article.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
+    describe('Method Save', function () {
+        it('should be able to save without problems', function (done) {
+            return article.save(function (err) {
+                should.not.exist(err);
+                done();
+            });
+        });
 
-		it('should be able to show an error when try to save without title', function(done) {
-			article.title = '';
+        it('should be able to show an error when try to save without title', function (done) {
+            article.title = '';
 
-			return article.save(function(err) {
-				should.exist(err);
-				done();
-			});
-		});
-	});
+            return article.save(function (err) {
+                should.exist(err);
+                done();
+            });
+        });
+    });
 
-	afterEach(function(done) {
-		Article.remove().exec(function() {
-			User.remove().exec(done);
-		});
-	});
+    afterEach(function (done) {
+        Article.remove().exec(function () {
+            User.remove().exec(done);
+        });
+    });
 });
