c3466347b4088f54bb94b3aa02abd51a38f302cf
Roberto Amador <robertoag89@gmail.com>
2015-06-26 12:37:21 -0500

    Update secure.js

--- config/env/secure.js
+++ config/env/secure.js
@@ -3,7 +3,7 @@
 module.exports = {
 	port: 8443,
 	db: {
-    		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
 		options: {
 			user: '',
 			pass: ''
