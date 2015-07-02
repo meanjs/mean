08b2f746d621bb16d487f5e0fe138d269e0c3551
Roberto <robertoag89@gmail.com>
2015-07-02 08:03:46 -0500

    Fig changed to Compose

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/mean-dev',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
 	app: {
 		title: 'MEAN.JS - Development Environment'
 	},
