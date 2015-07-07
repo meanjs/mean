73b5fa3df0bad170ba5ad3f8b118847e6ac199c6
Ryan Hutchison <ryan@gym.com>
2015-07-07 07:41:00 -0400

    Revert "load bootstrap (doh)"
    This reverts commit 7d8cea159bffd5cfe53d24dcb8d0d7af181cc2b1.

--- config/assets/default.js
+++ config/assets/default.js
@@ -8,8 +8,6 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css'
 			],
 			js: [
-				'public/lib/jquery/dist/jquery.js',
-				'public/lib/bootstrap/dist/js/bootstrap.js',
 				'public/lib/angular/angular.js',
 				'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-animate/angular-animate.js',
