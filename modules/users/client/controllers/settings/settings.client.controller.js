75c80524b75565e1a727a0ac42ec9403bea692dd
Ryan Hutchison <ryan@gym.com>
2015-07-09 14:23:31 -0400

    remove unused dependcies.

--- modules/users/client/controllers/settings/settings.client.controller.js
+++ modules/users/client/controllers/settings/settings.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('users').controller('SettingsController', ['$scope', '$http', '$location', 'Users', 'Authentication',
-	function($scope, $http, $location, Users, Authentication) {
+angular.module('users').controller('SettingsController', ['$scope', '$location', 'Authentication',
+	function($scope, $location, Authentication) {
 		$scope.user = Authentication.user;
 
 		// If user is not signed in then redirect back home
