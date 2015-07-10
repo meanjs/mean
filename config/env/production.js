61fda8d5bd90a3f7ca9b683a11f310a8e9dddd80
Ryan Hutchison <ryan@gym.com>
2015-07-10 07:21:47 -0400

    formatting: indent

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	secure: true,
-    port: process.env.PORT || 8443,
+	port: process.env.PORT || 8443,
 	db: {
 		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
 		options: {
