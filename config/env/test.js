2081987b990f0b38b62bac8360f74e28432c0d57
Luke Hamilton <luke@mettadata.com>
2015-07-19 13:05:12 +1000

    Set to get base title from default config.

--- config/env/test.js
+++ config/env/test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var defaultEnvConfig = require('./default');
+
 module.exports = {
 	db: {
 		uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-test',
@@ -10,7 +12,7 @@ module.exports = {
 	},
 	port: process.env.PORT || 3001,
 	app: {
-		title: 'MEAN.JS - Test Environment'
+		title: defaultEnvConfig.app.title + ' - Test Environment'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
