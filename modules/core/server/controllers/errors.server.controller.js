0efc82db0f749aa1f5120dfeaefe1b25365517d2
Igor Freire <igor.auad@gmail.com>
2015-07-20 14:38:44 -0300

    Fix message catching the field for which a duplicate key error ocurred

--- modules/core/server/controllers/errors.server.controller.js
+++ modules/core/server/controllers/errors.server.controller.js
@@ -7,7 +7,7 @@ var getUniqueErrorMessage = function(err) {
 	var output;
 
 	try {
-		var fieldName = err.err.substring(err.err.lastIndexOf('.$') + 2, err.err.lastIndexOf('_1'));
+		var fieldName = err.errmsg.substring(err.errmsg.lastIndexOf('.$') + 2, err.errmsg.lastIndexOf('_1'));
 		output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + ' already exists';
 
 	} catch(ex) {
