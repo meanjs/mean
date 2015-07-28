d5ea5c99d297bcd5c0fc7420cbd9294f839be703
cdriscol <drizkol@gmail.com>
2015-07-28 17:52:03 -0600

    Adding client test coverage.

--- modules/core/tests/client/header.client.controller.tests.js
+++ modules/core/tests/client/header.client.controller.tests.js
@@ -1,24 +1,64 @@
 'use strict';
 
 (function() {
-	describe('HeaderController', function() {
-		//Initialize global variables
-		var scope,
-			HeaderController;
+  describe('HeaderController', function() {
+    //Initialize global variables
+    var scope,
+      HeaderController,
+      $state,
+      Authentication;
 
-		// Load the main application module
-		beforeEach(module(ApplicationConfiguration.applicationModuleName));
+    // Load the main application module
+    beforeEach(module(ApplicationConfiguration.applicationModuleName));
 
-		beforeEach(inject(function($controller, $rootScope) {
-			scope = $rootScope.$new();
+    beforeEach(inject(function($controller, $rootScope, _$state_, _Authentication_) {
+      scope = $rootScope.$new();
+      $state = _$state_;
+      Authentication = _Authentication_;
 
-			HeaderController = $controller('HeaderController', {
-				$scope: scope
-			});
-		}));
+      HeaderController = $controller('HeaderController', {
+        $scope: scope
+      });
+    }));
 
-		it('should expose the authentication service', function() {
-			expect(scope.authentication).toBeTruthy();
-		});
-	});
+    it('should expose the authentication service', function() {
+      expect(scope.authentication).toBe(Authentication);
+    });
+
+    it('should expose the $state service', function() {
+      expect(scope.$state).toBe($state);
+    });
+
+    it('should default menu to collapsed', function() {
+      expect(scope.isCollapsed).toBeFalsy();
+    });
+
+    describe('when toggleCollapsibleMenu', function() {
+      var defaultCollapse;
+      beforeEach(function() {
+        defaultCollapse = scope.isCollapsed;
+        scope.toggleCollapsibleMenu();
+      });
+
+      it('should toggle isCollapsed to non default value', function() {
+        expect(scope.isCollapsed).not.toBe(defaultCollapse);
+      });
+
+      it('should then toggle isCollapsed back to default value', function() {
+        scope.toggleCollapsibleMenu();
+        expect(scope.isCollapsed).toBe(defaultCollapse);
+      });
+    });
+
+    describe('when view state changes', function() {
+      beforeEach(function() {
+        scope.isCollapsed = true;
+        scope.$broadcast('$stateChangeSuccess');
+      });
+
+      it('should set isCollapsed to false', function() {
+        expect(scope.isCollapsed).toBeFalsy();
+      });
+    });
+  });
 })();
--- modules/users/server/controllers/users/users.authorization.server.controller.js
+++ modules/users/server/controllers/users/users.authorization.server.controller.js
@@ -10,10 +10,16 @@ var _ = require('lodash'),
 /**
  * User middleware
  */
-exports.userByID = function(req, res, next, id) {
+exports.userByID = function (req, res, next, id) {
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(400).send({
+			message: 'User is invalid'
+		});
+	}
+
 	User.findOne({
 		_id: id
-	}).exec(function(err, user) {
+	}).exec(function (err, user) {
 		if (err) return next(err);
 		if (!user) return next(new Error('Failed to load User ' + id));
 		req.profile = user;
