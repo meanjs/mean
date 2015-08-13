360c3a4d3b66d3ee4a63043f4b3a08c04855d38a
Liran Tal <liran.tal@gmail.com>
2015-08-13 21:42:44 +0300

    Adding support for sessionKey configuration parameter to allow to easily change the session key that is used to hold the session value. The default is a generic sessionId key to introduce security through obscurity.

--- config/env/default.js
+++ config/env/default.js
@@ -9,7 +9,12 @@ module.exports = {
   },
   port: process.env.PORT || 3000,
   templateEngine: 'swig',
+  // Session details
+  // sessionSecret should be changed for security measures and concerns
   sessionSecret: 'MEAN',
+  // sessionKey is set to the generic sessionId key used by PHP applications
+  // for obsecurity reasons
+  sessionKey: 'sessionId',
   sessionCollection: 'sessions',
   logo: 'modules/core/img/brand/logo.png',
   favicon: 'modules/core/img/brand/favicon.ico'
