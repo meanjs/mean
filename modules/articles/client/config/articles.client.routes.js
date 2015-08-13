dced51b537a7e814645fc71cc037d9af5ea19835
Andrew Throener <athroener@gmail.com>
2015-08-13 15:10:10 -0500

    Article route / controller refactor

--- modules/articles/client/config/articles.client.routes.js
+++ modules/articles/client/config/articles.client.routes.js
@@ -8,26 +8,31 @@ angular.module('articles').config(['$stateProvider',
       .state('articles', {
         abstract: true,
         url: '/articles',
-        template: '<ui-view/>'
+        template: '<ui-view/>',
+        controller: 'ArticlesController'
       })
       .state('articles.list', {
         url: '',
-        templateUrl: 'modules/articles/views/list-articles.client.view.html'
+        templateUrl: 'modules/articles/views/list-articles.client.view.html',
+        controller: 'ArticlesListController'
       })
       .state('articles.create', {
         url: '/create',
         templateUrl: 'modules/articles/views/create-article.client.view.html',
+        controller: 'ArticlesCreateController',
         data: {
           roles: ['user', 'admin']
         }
       })
       .state('articles.view', {
         url: '/:articleId',
-        templateUrl: 'modules/articles/views/view-article.client.view.html'
+        templateUrl: 'modules/articles/views/view-article.client.view.html',
+        controller: 'ArticlesViewController'
       })
       .state('articles.edit', {
         url: '/:articleId/edit',
         templateUrl: 'modules/articles/views/edit-article.client.view.html',
+        controller: 'ArticlesEditController',
         data: {
           roles: ['user', 'admin']
         }
--- modules/articles/client/controllers/articles.client.controller.js
+++ modules/articles/client/controllers/articles.client.controller.js
@@ -1,10 +1,58 @@
 'use strict';
 
 // Articles controller
-angular.module('articles').controller('ArticlesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles',
-  function ($scope, $stateParams, $location, Authentication, Articles) {
-    $scope.authentication = Authentication;
+angular.module('articles')
+.controller('ArticlesListController', ['$scope', '$state', 'Articles',
+  function ($scope, $stateParams, Articles) {
+    //Query Articles
+    $scope.articles = Articles.query();
+  }
+])
+.controller('ArticlesViewController', ['$scope', '$state', '$stateParams', 'Articles',
+  function ($scope, $state, $stateParams, Articles) {
+    //Check if article is loaded if not load it
+    if ($scope.article === undefined || typeof $scope.article !== 'object' || $scope.article._id === undefined || $scope.article._id !== $stateParams.articleId) {
+      $scope.loadArticleById($stateParams.articleId);
+    }
 
+    // Remove existing Article
+    $scope.remove = function () {
+      $scope.authCheck();
+      if (confirm('Are you sure you want to delete?')) {
+        $scope.article.$remove(function (article) {
+          if ($scope.articles !== undefined && $scope.articles.length > 0) {
+            $scope.articles.splice($scope.articles.indexOf($scope.article), 1);
+          }
+          $state.go('articles.list');
+        });
+      }
+    };
+  }
+])
+.controller('ArticlesEditController', ['$scope', '$state', '$stateParams', 'Authentication', 'Articles',
+  function ($scope, $state, $stateParams, Authentication, Articles) {
+
+    //Check if article is loaded if not load it
+    if ($scope.article === undefined || typeof $scope.article !== 'object' || $scope.article._id === undefined || $scope.article._id !== $stateParams.articleId) {
+      $scope.loadArticleById($stateParams.articleId);
+    }
+
+    //Auth Check on Edit Route
+    $scope.authCheck();
+
+    $scope.update = function () {
+      var article = $scope.article;
+
+      article.$update(function () {
+        $state.go('articles.view',{articleId: article._id});
+      }, function (errorResponse) {
+        $scope.error = errorResponse.data.message;
+      });
+    };
+  }
+])
+.controller('ArticlesCreateController', ['$scope', '$state', '$stateParams', 'Authentication', 'Articles',
+  function ($scope, $state, $stateParams, Authentication, Articles) {
     // Create new Article
     $scope.create = function () {
       // Create new Article object
@@ -15,7 +63,7 @@ angular.module('articles').controller('ArticlesController', ['$scope', '$statePa
 
       // Redirect after save
       article.$save(function (response) {
-        $location.path('articles/' + response._id);
+        $state.go('articles.list', {articleId: response._id});
 
         // Clear form fields
         $scope.title = '';
@@ -24,45 +72,35 @@ angular.module('articles').controller('ArticlesController', ['$scope', '$statePa
         $scope.error = errorResponse.data.message;
       });
     };
+  }
+])
+.controller('ArticlesController', ['$scope', '$stateParams', '$state', 'Authentication', 'Articles',
+  function ($scope, $stateParams, $state, Authentication, Articles) {
 
-    // Remove existing Article
-    $scope.remove = function (article) {
-      if (article) {
-        article.$remove();
-
-        for (var i in $scope.articles) {
-          if ($scope.articles[i] === article) {
-            $scope.articles.splice(i, 1);
-          }
-        }
-      } else {
-        $scope.article.$remove(function () {
-          $location.path('articles');
-        });
-      }
-    };
-
-    // Update existing Article
-    $scope.update = function () {
-      var article = $scope.article;
-
-      article.$update(function () {
-        $location.path('articles/' + article._id);
-      }, function (errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
-    };
-
-    // Find a list of Articles
-    $scope.find = function () {
-      $scope.articles = Articles.query();
-    };
-
-    // Find existing Article
-    $scope.findOne = function () {
+    $scope.loadArticleById = function (id) {
       $scope.article = Articles.get({
-        articleId: $stateParams.articleId
+        articleId: id
+      });
+
+      $scope.article.$promise.then(function (article) {
+        if (article === undefined || typeof article !== 'object' || article._id === undefined) {
+          //Article not found
+          $state.go('not-found');
+        }
       });
     };
+
+    $scope.authCheck = function () {
+      $scope.article.$promise.then(function (article) {
+        //Make sure user is authorized if not on the view state
+        if ($scope.article.user._id !== Authentication.user._id) {
+          //TODO  Change to unauthorized when that PR is merged
+          $state.go('articles.list');
+        }
+      });
+    };
+
+    //Not sure if this is needed anymore?
+    $scope.authentication = Authentication;
   }
 ]);
