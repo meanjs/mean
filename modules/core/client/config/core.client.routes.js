9fdd2a0b9c279b72b57ab1ed95137165dbf70fbd
Ryan Hutchison <ryan@gym.com>
2015-08-23 03:59:43 -0400

    mask exception route, but show not-found page.

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -5,7 +5,11 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
   function ($stateProvider, $urlRouterProvider) {
 
     // Redirect to 404 when route not found
-    $urlRouterProvider.otherwise('not-found');
+    $urlRouterProvider.otherwise(function ($injector, $location) {
+      $injector.get('$state').transitionTo('not-found', null, {
+        location: false
+      });
+    });
 
     // Home state routing
     $stateProvider
