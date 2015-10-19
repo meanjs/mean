f7524bbc17349c5afc4bce96b8fe36e35fe64f44
Ivan Morales <ivan@imorales.com>
2015-10-19 09:48:48 -0500

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: 'mongodb://cm-reviews:cmreviews1@ds041144.mongolab.com:41144/cm-reviews-dev',
     options: {
       user: '',
       pass: ''
