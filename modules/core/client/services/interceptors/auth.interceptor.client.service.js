680e26822f84aa5f045d88be91841d67cf9f68b2
Nacho García Fernández <nachogarcia91@gmail.com>
2015-11-09 11:38:46 +0100

    Update auth.interceptor.client.service.js
    Included 400 and 404 error routes.

--- modules/core/client/services/interceptors/auth.interceptor.client.service.js
+++ modules/core/client/services/interceptors/auth.interceptor.client.service.js
@@ -6,11 +6,17 @@ angular.module('core').factory('authInterceptor', ['$q', '$injector',
       responseError: function(rejection) {
         if (!rejection.config.ignoreAuthModule) {
           switch (rejection.status) {
+            case 400:
+              $injector.get('$state').transitionTo('bad-request');
+              break;
             case 401:
               $injector.get('$state').transitionTo('authentication.signin');
               break;
             case 403:
               $injector.get('$state').transitionTo('forbidden');
+              break;
+            case 404:
+              $injector.get('$state').transitionTo('not-found');
               break;
           }
         }
