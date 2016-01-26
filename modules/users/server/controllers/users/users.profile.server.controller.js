200a6e78f20636b0c01967e0f26d41d6c090b1d8
James Ireland <james@siteshackinc.com>
2016-01-25 21:35:24 -0600

    Update users.profile.server.controller.js
    If user changes their email for their username lets also update the email

--- modules/users/server/controllers/users/users.profile.server.controller.js
+++ modules/users/server/controllers/users/users.profile.server.controller.js
@@ -27,6 +27,7 @@ exports.update = function (req, res) {
     user = _.extend(user, req.body);
     user.updated = Date.now();
     user.displayName = user.firstName + ' ' + user.lastName;
+    user.email = user.username;
 
     user.save(function (err) {
       if (err) {
