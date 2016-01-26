302842545f3bd76270e7428f6f12dc0e15fd8475
James Ireland <james@siteshackinc.com>
2016-01-25 21:54:06 -0600

    Update default.js
    Added additional scripts

--- config/assets/default.js
+++ config/assets/default.js
@@ -4,19 +4,28 @@ module.exports = {
   client: {
     lib: {
       css: [
-        'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap.min.css',
+        'public/lib/WebFont-OpenSans/css/stylesheet.css',
+        'public/lib/font-awesome/css/font-awesome.css'
       ],
       js: [
-        'public/lib/angular/angular.js',
-        'public/lib/angular-resource/angular-resource.js',
-        'public/lib/angular-animate/angular-animate.js',
-        'public/lib/angular-messages/angular-messages.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.js',
-        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/bootstrap/dist/js/bootstrap.min.js',
+        'public/lib/summernote/dist/summernote.min.js',
+        'public/lib/moment/moment.js',
+        'public/lib/angular/angular.min.js',
+        'public/lib/angular-resource/angular-resource.min.js',
+        'public/lib/angular-animate/angular-animate.min.js',
+        'public/lib/angular-messages/angular-messages.min.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/angular-ui-utils/ui-utils.min.js',
+        'public/lib/angular-sanitize/angular-sanitize.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/angular-file-upload/angular-file-upload.min.js',
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/bootstrap-ui-datetime-picker/dist/datetime-picker.min.js',
+        'public/lib/angular-summernote/dist/angular-summernote.min.js',
+        'public/lib/angular-moment/angular-moment.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
