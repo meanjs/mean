9bd847a95f93cd54f2f39b64bce1f92f534194f1
James Ireland <james@siteshackinc.com>
2016-01-25 21:25:28 -0600

    Update list.articles.client.controller.js

--- modules/articles/client/controllers/list.articles.client.controller.js
+++ modules/articles/client/controllers/list.articles.client.controller.js
@@ -5,11 +5,33 @@
     .module('articles')
     .controller('ArticlesListController', ArticlesListController);
 
-  ArticlesListController.$inject = ['ArticlesService'];
-
-  function ArticlesListController(ArticlesService) {
+  ArticlesListController.$inject = ['ArticlesService', $filter) {
     var vm = this;
 
-    vm.articles = ArticlesService.query();
+    ArticlesService.query(function (data) {
+      vm.articles = data;
+      vm.buildPager();
+    });
+
+    vm.buildPager = function () {
+      vm.pagedItems = [];
+      vm.itemsPerPage = 5;
+      vm.currentPage = 1;
+      vm.figureOutItemsToDisplay();
+    };
+
+    vm.figureOutItemsToDisplay = function () {
+      vm.filteredItems = $filter('filter')(vm.articles, {
+        $: vm.search
+      });
+      vm.filterLength = vm.filteredItems.length;
+      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+      var end = begin + vm.itemsPerPage;
+      vm.pagedItems = vm.filteredItems.slice(begin, end);
+    };
+
+    vm.pageChanged = function () {
+      vm.figureOutItemsToDisplay();
+    };
   }
 })();
