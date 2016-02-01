aaf24799ce0fe4667e0c803b596a646ce01c4d3e
Andrew Throener <athroener@gmail.com>
2016-02-01 16:50:52 -0600

    Optionize JwtAuth function

--- config/lib/jwtAuthentication.js
+++ config/lib/jwtAuthentication.js
@@ -1,22 +1,33 @@
 'use strict';
 
 var config = require('../config'),
-  jwt = require('jsonwebtoken');
+  jwt = require('jsonwebtoken'),
+  lodash = require('lodash');
 
 
 
 // export the token auth service
-exports.signToken = function (user) {
-  var payload = {
-    user: user._id,
-    expiration: Date.now() + (config.jwt.expiresInSeconds * 1000)
+exports.signToken = function (user, options) {
+  var payload,
+    defaults,
+    token,
+    jwtOptions;
+
+  payload = {
+    user: user._id
   };
 
-  var options = {
+  defaults = {
     expiresIn: config.jwt.expiresInSeconds
   };
 
-  var token = jwt.sign(payload, config.jwt.secret, options);
+  if(options) {
+    jwtOptions = lodash.merge(defaults, options);
+  } else {
+    jwtOptions = defaults;
+  }
+
+  token = jwt.sign(payload, config.jwt.secret, jwtOptions);
 
   return token;
 };
