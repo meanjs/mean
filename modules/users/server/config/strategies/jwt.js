fe7b75121b4dbc1b6757a3069f1e0beccd97e179
Andrew Throener <athroener@gmail.com>
2016-02-03 12:08:54 -0600

    Bug fix: password/salt null after profile update

--- modules/users/server/config/strategies/jwt.js
+++ modules/users/server/config/strategies/jwt.js
@@ -14,15 +14,13 @@ module.exports = function (config) {
   //opts.issuer = "accounts.examplesoft.com";
   //opts.audience = "yoursite.net";
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findById({ _id: jwt_payload.user }, function(err, user) {
+    User.findById({ _id: jwt_payload.user }, '-salt -password', function(err, user) {
       if (err) {
         return done(err, false);
       }
 
       if (user) {
-        //Protect salt/password
-        user.salt = null;
-        user.password = null;
+        
 
         return done(null, user);
       }
