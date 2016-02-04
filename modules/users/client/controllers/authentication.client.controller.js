3e7dcdc265755cdef375fc5837cb2ea49a44723f
Andrew Throener <athroener@gmail.com>
2016-02-04 10:17:02 -0600

    removed console.log

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -43,8 +43,6 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
         return false;
       }
 
-      console.log($scope);
-
       $http.post('/api/auth/signin', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
