51726986c9df035b106fb96fdbbcb52c393fb68b
Ryan Hutchison <ryan@gym.com>
2016-02-10 00:35:48 -0500

    fix(users): Update assertion for duplicate username/email for newer versions of mongodb.
    When running a newer version of mongodb, users are reporting an issue when executing tests because the response from the mongo request has changed.  This change should support new/old versions of the test.
    
    Resolves #1124

--- modules/users/tests/server/user.server.routes.tests.js
+++ modules/users/tests/server/user.server.routes.tests.js
@@ -742,7 +742,7 @@ describe('User CRUD tests', function () {
               }
 
               // Call the assertion callback
-              userInfoRes.body.message.should.equal('Username already exists');
+              userInfoRes.body.message.toLowerCase().should.containEql('username already exists');
 
               return done();
             });
@@ -794,7 +794,7 @@ describe('User CRUD tests', function () {
               }
 
               // Call the assertion callback
-              userInfoRes.body.message.should.equal('Email already exists');
+              userInfoRes.body.message.toLowerCase().should.containEql('email already exists');
 
               return done();
             });
