b2db312e14a75d8fb45c6ffc28368786e9a14901
Yilen <yilen.pan@gmail.com>
2016-02-15 00:21:46 -0800

    feat(csrf): Added Lusca CSRF Prevention
    Added lusca for CSRF protection as per issue #828
    
    Fixes #828

--- config/env/default.js
+++ config/env/default.js
@@ -28,6 +28,19 @@ module.exports = {
   // for obsecurity reasons
   sessionKey: 'sessionId',
   sessionCollection: 'sessions',
+  // Lusca config
+  csrf: {
+    csrf: false,
+    csp: { /* Content Security Policy object */},
+    xframe: 'SAMEORIGIN',
+    p3p: 'ABCDEF',
+    hsts: {
+      maxAge: 31536000, // Forces HTTPS for one year
+      includeSubDomains: true,
+      preload: true
+    },
+    xssProtection: true
+  },
   logo: 'modules/core/client/img/brand/logo.png',
   favicon: 'modules/core/client/img/brand/favicon.ico',
   uploads: {
