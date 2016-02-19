157bacb1615a7053ba1880717793f8a3b08fa486
IteloFilho <itelofilho@gmail.com>
2016-02-19 10:46:18 -0300

    fix(core): fix page-title directive

---
+++ modules/core/client/directives/page-title.client.directive.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular.module('core')
+    .directive('pageTitle', pageTitle);
+
+  pageTitle.$inject = ['$rootScope', '$interpolate', '$state'];
+
+  function pageTitle($rootScope, $interpolate, $state) {
+    var directive = {
+      retrict: 'A',
+      link: link
+    };
+
+    return directive;
+
+    function link(scope, element) {
+      $rootScope.$on('$stateChangeSuccess', listener);
+
+      function listener(event, toState) {
+        var applicationCoreTitle = 'MEAN.js',
+          separeteBy = ' - ';
+        if (toState.data && toState.data.pageTitle) {
+          var stateTitle = $interpolate(toState.data.pageTitle)($state.$current.locals.globals);
+          element.html(applicationCoreTitle + separeteBy + stateTitle);
+        } else {
+          element.html(applicationCoreTitle);
+        }
+      }
+    }
+  }
+})();
