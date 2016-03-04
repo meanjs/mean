d30ba5a15127c7ad97eedf9b2577a39a2db8bdb3
jaganttp <jaganttp@gmail.com>
2016-03-03 16:23:18 -0800

    Update default.js

--- config/env/default.js
+++ config/env/default.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    title: 'AppName',
+    description: 'You Application Description..',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
