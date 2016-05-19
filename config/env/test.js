5b9b2fdee63c90f97864f8cbb9d131ab455abd8b
Liran Tal <liran.tal@gmail.com>
2016-05-19 11:50:14 +0300

    only enabling morgan logger if config.log.format option was defined, and disabling the app.log file transport option for the test environment

--- config/env/test.js
+++ config/env/test.js
@@ -15,14 +15,14 @@ module.exports = {
   log: {
     // logging with Morgan - https://github.com/expressjs/morgan
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
-    format: 'combined',
-    fileLogger: {
-      directoryPath: process.cwd(),
-      fileName: 'app.log',
-      maxsize: 10485760,
-      maxFiles: 2,
-      json: false
-    }
+    format: 'dev'
+    // fileLogger: {
+    //   directoryPath: process.cwd(),
+    //   fileName: 'app.log',
+    //   maxsize: 10485760,
+    //   maxFiles: 2,
+    //   json: false
+    // }
   },
   port: process.env.PORT || 3001,
   app: {
