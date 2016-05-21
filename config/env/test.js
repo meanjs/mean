6e2721c111a25ffb7207640cde5f1c8a2267bd6e
Liran Tal <liran.tal@gmail.com>
2016-05-21 22:31:20 +0300

    disabling all kind of logging when in test enviroment

--- config/env/test.js
+++ config/env/test.js
@@ -15,7 +15,7 @@ module.exports = {
   log: {
     // logging with Morgan - https://github.com/expressjs/morgan
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
-    format: 'dev'
+    // format: 'dev'
     // fileLogger: {
     //   directoryPath: process.cwd(),
     //   fileName: 'app.log',
