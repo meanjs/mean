64392b1eeda186aabae12742aedde1f0c5804e6f
Liran Tal <liran.tal@gmail.com>
2016-07-31 13:34:04 +0300

    fix(config): updating environment variable name for mongodb with the new one for Heroku (#1413)

--- config/env/production.js
+++ config/env/production.js
@@ -11,7 +11,7 @@ module.exports = {
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
       user: '',
       pass: ''
