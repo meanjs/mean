e05be44ffb6dc2bc0a750cc5576d7a0641c804c7
aanev <assen@xquadro.com>
2016-08-06 16:56:18 +0300

    fix(user): fix changeProfilePicture
    * use async to simplify callbacks
    
    * use fs.unlink to delete old picture once the profile is updated
    
    Fixes #1415

--- modules/users/server/controllers/users/users.profile.server.controller.js
+++ modules/users/server/controllers/users/users.profile.server.controller.js
@@ -9,6 +9,7 @@ var _ = require('lodash'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
   mongoose = require('mongoose'),
   multer = require('multer'),
+  async = require('async'),
   config = require(path.resolve('./config/config')),
   User = mongoose.model('User');
 
@@ -60,36 +61,55 @@ exports.changeProfilePicture = function (req, res) {
   var user = req.user;
   var upload = multer(config.uploads.profileUpload).single('newProfilePicture');
   var profileUploadFileFilter = require(path.resolve('./config/lib/multer')).profileUploadFileFilter;
+  var tmpImageUrl;
 
   // Filtering to upload only images
   upload.fileFilter = profileUploadFileFilter;
 
   if (user) {
-    upload(req, res, function (uploadError) {
-      if (uploadError) {
-        return res.status(400).send({
-          message: 'Error occurred while uploading profile picture'
-        });
-      } else {
-        user.profileImageURL = config.uploads.profileUpload.dest + req.file.filename;
-
-        user.save(function (saveError) {
-          if (saveError) {
-            return res.status(400).send({
-              message: errorHandler.getErrorMessage(saveError)
+    async.series({
+      uploadImage: function (callback) {
+        upload(req, res, function (uploadError) {
+          if (uploadError) {
+            callback({
+              message: 'Error occurred while uploading profile picture'
             });
           } else {
-            req.login(user, function (err) {
-              if (err) {
-                res.status(400).send(err);
-              } else {
-                res.json(user);
-              }
-            });
+            callback(null);
           }
         });
+      },
+      saveUser: function(callback) {
+        tmpImageUrl = user.profileImageURL;
+        user.profileImageURL = config.uploads.profileUpload.dest + req.file.filename;
+        user.save(callback);
+      },
+      deleteOldImage: function (callback) {
+        if (tmpImageUrl !== User.schema.path('profileImageURL').defaultValue) {
+          fs.unlink(tmpImageUrl, function (unlinkError) {
+            if (unlinkError) {
+              callback({
+                message: 'Error occurred while uploading profile picture'
+              });
+            } else {
+              callback(null);
+            }
+          });
+        } else {
+          callback(null);
+        }
+      },
+      login: function (callback) {
+        req.login(user, callback);
+      }
+    }, function(err) {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.json(user);
       }
     });
+
   } else {
     res.status(400).send({
       message: 'User is not signed in'
