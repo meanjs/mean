8ac5a64edd07eb891b1738eddf737c0f5bee3d81
Mikael Korpela <mikael@ihminen.org>
2016-08-15 11:16:46 +0300

    fix(user): specify a digest with crypto.pbkdf2Sync
    Fixes Node v6 crypto deprecation warning  “crypto.pbkdf2 without specifying a digest is deprecated. Please specify a digest”

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -135,7 +135,7 @@ UserSchema.pre('validate', function (next) {
  */
 UserSchema.methods.hashPassword = function (password) {
   if (this.salt && password) {
-    return crypto.pbkdf2Sync(password, new Buffer(this.salt, 'base64'), 10000, 64).toString('base64');
+    return crypto.pbkdf2Sync(password, new Buffer(this.salt, 'base64'), 10000, 64, 'SHA1').toString('base64');
   } else {
     return password;
   }
