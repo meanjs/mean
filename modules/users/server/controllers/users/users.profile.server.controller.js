a02088f820562f217f08c23f561c649aca0ac5aa
aanev <assen@xquadro.com>
2016-08-26 10:10:02 +0300

    fix(user): fix changeProfilePicture to overwrite existing files
    * fix(user): fix changeProfilePicture
    
    * use promises to simplify callbacks
    
    * use fs.unlink to delete old picture once the profile is updated
    
    Fixes #1415
    
    * fix(user): fix changeProfilePicture
    
    * use promises to simplify callbacks
    
    * use fs.unlink to delete old picture once the profile is updated
    
    * log file errors to console
    
    Fixes #1415
    
    * fix(user): fix changeProfilePicture
    
    * use promises to simplify callbacks
    
    * use fs.unlink to delete old picture once the profile is updated
    
    * log file errors to console
    
    * update error handler module to handle file upload errors
    
    Fixes #1415
    
    * fix(user): fix changeProfilePicture
    
    * use promises to simplify callbacks
    
    * use fs.unlink to delete old picture once the profile is updated
    
    * log file errors to console
    
    * update error handler module to handle file upload errors
    
    Fixes #1415

--- modules/users/server/controllers/users/users.profile.server.controller.js
+++ modules/users/server/controllers/users/users.profile.server.controller.js
@@ -60,39 +60,82 @@ exports.changeProfilePicture = function (req, res) {
   var user = req.user;
   var upload = multer(config.uploads.profileUpload).single('newProfilePicture');
   var profileUploadFileFilter = require(path.resolve('./config/lib/multer')).profileUploadFileFilter;
+  var existingImageUrl;
 
   // Filtering to upload only images
   upload.fileFilter = profileUploadFileFilter;
 
   if (user) {
-    upload(req, res, function (uploadError) {
-      if (uploadError) {
-        return res.status(400).send({
-          message: 'Error occurred while uploading profile picture'
-        });
-      } else {
-        user.profileImageURL = config.uploads.profileUpload.dest + req.file.filename;
-
-        user.save(function (saveError) {
-          if (saveError) {
-            return res.status(400).send({
-              message: errorHandler.getErrorMessage(saveError)
-            });
-          } else {
-            req.login(user, function (err) {
-              if (err) {
-                res.status(400).send(err);
-              } else {
-                res.json(user);
-              }
-            });
-          }
-        });
-      }
-    });
+    existingImageUrl = user.profileImageURL;
+    uploadImage()
+      .then(updateUser)
+      .then(deleteOldImage)
+      .then(login)
+      .then(function () {
+        res.json(user);
+      })
+      .catch(function (err) {
+        res.status(400).send(err);
+      });
   } else {
     res.status(400).send({
       message: 'User is not signed in'
+    });
+  }
+
+  function uploadImage () {
+    return new Promise(function (resolve, reject) {
+      upload(req, res, function (uploadError) {
+        if (uploadError) {
+          reject(errorHandler.getErrorMessage(uploadError));
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  function updateUser () {
+    return new Promise(function (resolve, reject) {
+      user.profileImageURL = config.uploads.profileUpload.dest + req.file.filename;
+      user.save(function (err, theuser) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  function deleteOldImage () {
+    return new Promise(function (resolve, reject) {
+      if (existingImageUrl !== User.schema.path('profileImageURL').defaultValue) {
+        fs.unlink(existingImageUrl, function (unlinkError) {
+          if (unlinkError) {
+            console.log(unlinkError);
+            reject({
+              message: 'Error occurred while deleting old profile picture'
+            });
+          } else {
+            resolve();
+          }
+        });
+      } else {
+        resolve();
+      }
+    });
+  }
+
+  function login () {
+    return new Promise(function (resolve, reject) {
+      req.login(user, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
     });
   }
 };
