14136450ab70a12a0eccbacac0850820cc398590
hyperreality <hyperreality@users.noreply.github.com>
2016-08-29 23:10:14 +0100

    feat(core): update project dependencies, fix express config (#1463)

--- config/lib/express.js
+++ config/lib/express.js
@@ -53,12 +53,6 @@ module.exports.initLocalVariables = function (app) {
  * Initialize application middleware
  */
 module.exports.initMiddleware = function (app) {
-  // Showing stack errors
-  app.set('showStackError', true);
-
-  // Enable jsonp
-  app.enable('jsonp callback');
-
   // Should be placed before express.static
   app.use(compress({
     filter: function (req, res) {
@@ -146,10 +140,10 @@ module.exports.initModulesConfiguration = function (app, db) {
 module.exports.initHelmetHeaders = function (app) {
   // Use helmet to secure Express headers
   var SIX_MONTHS = 15778476000;
-  app.use(helmet.xframe());
+  app.use(helmet.frameguard());
   app.use(helmet.xssFilter());
-  app.use(helmet.nosniff());
-  app.use(helmet.ienoopen());
+  app.use(helmet.noSniff());
+  app.use(helmet.ieNoOpen());
   app.use(helmet.hsts({
     maxAge: SIX_MONTHS,
     includeSubdomains: true,
