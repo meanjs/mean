ddd3d8631d3d2b6837814d3a2c1c823c98a73ca6
Daron Jones <daronalerts@gmail.com>
2016-09-07 17:01:42 +0100

    feat(views) configure handlebars in separte file
    - Moved setup of handlebars to it's own config file.
    - Added helper for parsing JSON in handlebars
    
    Should make adding helpers easier and keep config/libexpress.js leaner

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -24,7 +24,7 @@ exports.renderIndex = function (req, res) {
   }
 
   res.render('modules/core/server/views/index', {
-    user: JSON.stringify(safeUserObject)
+    user: safeUserObject
   });
 };
 
