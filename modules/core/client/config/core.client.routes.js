c7a2f3ab006227d9f6cd90e9fbf54fb25d058504
hyperreality <lmt@fastmail.com>
2016-09-15 11:17:07 -0400

    Added 400 and 404 custom error messages

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -21,9 +21,7 @@
 
     // Redirect to 404 when route not found
     $urlRouterProvider.otherwise(function ($injector, $location) {
-      $injector.get('$state').transitionTo('not-found', null, {
-        location: false
-      });
+      $injector.get('$state').transitionTo('not-found');
     });
 
     $stateProvider
@@ -36,17 +34,31 @@
       .state('not-found', {
         url: '/not-found',
         templateUrl: 'modules/core/client/views/404.client.view.html',
+        controller: 'ErrorController',
+        controllerAs: 'vm',
+        params: {
+          message: function($stateParams) {
+            return $stateParams.message;
+          }
+        },
         data: {
           ignoreState: true,
-          pageTitle: 'Not-Found'
+          pageTitle: 'Not Found'
         }
       })
       .state('bad-request', {
         url: '/bad-request',
         templateUrl: 'modules/core/client/views/400.client.view.html',
+        controller: 'ErrorController',
+        controllerAs: 'vm',
+        params: {
+          message: function($stateParams) {
+            return $stateParams.message;
+          }
+        },
         data: {
           ignoreState: true,
-          pageTitle: 'Bad-Request'
+          pageTitle: 'Bad Request'
         }
       })
       .state('forbidden', {
--- modules/core/client/services/interceptors/auth-interceptor.client.service.js
+++ modules/core/client/services/interceptors/auth-interceptor.client.service.js
@@ -17,6 +17,9 @@
     function responseError(rejection) {
       if (!rejection.config.ignoreAuthModule) {
         switch (rejection.status) {
+          case 400:
+            $injector.get('$state').go('bad-request', { message: rejection.data.message });
+            break;
           case 401:
             // Deauthenticate the global user
             Authentication.user = null;
@@ -24,6 +27,9 @@
             break;
           case 403:
             $injector.get('$state').transitionTo('forbidden');
+            break;
+          case 404:
+            $injector.get('$state').go('not-found', { message: rejection.data.message });
             break;
         }
       }
