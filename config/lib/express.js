67783a67bb76aebf4e97787516af33f1b94b84bb
Sujeeth Kolluru <sujeethkv@yahoo.com>
2016-09-30 17:30:09 -0400

    feat(core): Add prod asset versioning
    Asset versioning for prod - idea from #1509
    
    Fixes #1509

--- config/lib/express.js
+++ config/lib/express.js
@@ -158,7 +158,7 @@ module.exports.initHelmetHeaders = function (app) {
  */
 module.exports.initModulesClientRoutes = function (app) {
   // Setting the app router and static folder
-  app.use('/', express.static(path.resolve('./public')));
+  app.use('/', express.static(path.resolve('./public'), { maxAge: 86400000 }));
 
   // Globbing static routing
   config.folders.client.forEach(function (staticPath) {
