b0c5ad9f692d1665568564ee8a81d4306f3fc4f2
Sujeeth <sujeethkv@yahoo.com>
2016-10-06 11:48:43 -0400

    feat(core): Add prod asset versioning (#1536)
    Asset versioning for prod - idea from #1509
    
    Fixes #1509

--- config/assets/production.js
+++ config/assets/production.js
@@ -9,6 +9,7 @@ module.exports = {
         // bower:css
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/ng-img-crop/compile/minified/ng-img-crop.css',
         // endbower
       ],
       js: [
@@ -16,16 +17,17 @@ module.exports = {
         'public/lib/angular/angular.min.js',
         'public/lib/angular-animate/angular-animate.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
         'public/lib/angular-messages/angular-messages.min.js',
         'public/lib/angular-mocks/angular-mocks.js',
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/ng-file-upload/ng-file-upload.min.js',
+        'public/lib/ng-img-crop/compile/minified/ng-img-crop.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
         // endbower
       ]
     },
-    css: 'public/dist/application.min.css',
-    js: 'public/dist/application.min.js'
+    css: 'public/dist/application*.min.css',
+    js: 'public/dist/application*.min.js'
   }
 };
