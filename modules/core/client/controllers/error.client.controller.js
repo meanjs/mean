6be12f8a061a2f7228e254c314a46ad7a78966d1
Michael Leanos <michael.leanos@gmail.com>
2016-10-07 22:03:31 -0700

    fix(core): Add custom 400 and 404 error messages (#1547)
    * Added 400 and 404 custom error messages
    
    * nicer error message views
    
    * Sign Up & Sign In error responses
    
    Changed the error responses returned from the Sign Up & Sign In API
    calls to use 422 rather than 400.
    
    For insight into why this change was made:
    https://github.com/meanjs/mean/pull/1510#issuecomment-247435378
    
    For reference on why to use 422 over 400:
    https://www.bennadel.com/blog/2434-http-status-codes-for-invalid-data-400-vs-422.htm

---
+++ modules/core/client/controllers/error.client.controller.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('core')
+    .controller('ErrorController', ErrorController);
+
+  ErrorController.$inject = ['$stateParams'];
+
+  function ErrorController($stateParams) {
+    var vm = this;
+    vm.errorMessage = null;
+
+    // Display custom message if it was set
+    if ($stateParams.message) vm.errorMessage = $stateParams.message;
+  }
+}());
+
