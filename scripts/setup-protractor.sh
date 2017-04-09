73135df49fbf14e966b951a4586ce77bf84e5176
Liran Tal <liran.tal@gmail.com>
2017-04-09 08:40:48 +0300

    fix(protractor): support for gulp-protractor for v6-7 only (#1752)
    * fix(protractor): updating gulp-protractor
    * re-factoring out protractor prep tasks

---
+++ scripts/setup-protractor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+echo "$TRAVIS_NODE_VERSION"
+if [[ "$TRAVIS_NODE_VERSION" == "6" || "$TRAVIS_NODE_VERSION" == "7" ]]
+then
+  npm install protractor
+  export DISPLAY=:99.0
+  bash -e /etc/init.d/xvfb start
+  ./node_modules/protractor/bin/webdriver-manager update --standalone --firefox
+  ./node_modules/protractor/bin/webdriver-manager start 2>&1 &
+fi
