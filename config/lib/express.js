25d7585cde1796bf0b036ee79efce650067c1439
| <liran.tal@gmail.com>
2017-06-29 17:20:29 +0300

    fix(helmet): updating to support latest changes, transition from ms to seconds represntation

--- config/lib/express.js
+++ config/lib/express.js
@@ -141,14 +141,13 @@ module.exports.initModulesConfiguration = function (app, db) {
  */
 module.exports.initHelmetHeaders = function (app) {
   // Use helmet to secure Express headers
-  var SIX_MONTHS = 15778476000;
+  var SIX_MONTHS = 15778476;
   app.use(helmet.frameguard());
   app.use(helmet.xssFilter());
   app.use(helmet.noSniff());
   app.use(helmet.ieNoOpen());
   app.use(helmet.hsts({
     maxAge: SIX_MONTHS,
-    includeSubdomains: true,
     force: true
   }));
   app.disable('x-powered-by');
