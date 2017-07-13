16a1dcd1d37894130d33f61914f05f1ebc1ac5b1
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-07-13 19:57:09 +0200

    feat(dependencies): replace PhantomJS in favor of chrome (#1776)
    * fix(users): test for usernameOrEmail
    
    * Add comment to remind  change for mongo replicaset connection
    
    * clean comment ..
    
    * Generic pageTitle concept
    
    * Revert "Generic pageTitle concept"
    
    This reverts commit ff00ec950f085ca3b6d1abb564eab1965ab0a56e.
    
    * align on meanjs state
    
    * fix atom beautify newline
    
    * align to mean indent
    
    * upgrade protractor
    
    * Switch from firefox to chrome for travis
    
    * fix build
    
    * clean npm install protractor
    
    * enable sudo
    
    * clean order for mean
    
    * clean dependencies

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,15 +5,4 @@ var config = {
   specs: ['modules/*/tests/e2e/*.js']
 };
 
-if (process.env.TRAVIS) {
-  config.capabilities = {
-    // Without this setting, Travis CI would default
-    // to using Chrome anyway.
-    // NOTE: Firefox is currently not working with
-    // the Travis CI builds. For more info see:
-    // https://github.com/meanjs/mean/pull/1805
-    browserName: 'chrome'
-  };
-}
-
 exports.config = config;
