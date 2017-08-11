cca18279080bebaf7d4047f6f7b14c5fee1a80d2
Mikael Korpela <mikael@ihminen.org>
2017-08-11 12:29:02 +0300

    feat(build): Turn on mangling for uglify
    Previously this might've caused issues with AngularJS, but since we are now using ngAnnotate, those issues are gone.
    
    https://github.com/mishoo/UglifyJS2#minify-options

--- gulpfile.js
+++ gulpfile.js
@@ -153,7 +153,7 @@ gulp.task('uglify', function () {
   return gulp.src(assets)
     .pipe(plugins.ngAnnotate())
     .pipe(plugins.uglify({
-      mangle: false
+      mangle: true
     }))
     .pipe(plugins.concat('application.min.js'))
     .pipe(plugins.rev())
