11c666d579e955d4abbb62ee63fda953f5f2ae6f
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-08-30 10:40:41 +0200

    Display err of uglify in console

--- gulpfile.js
+++ gulpfile.js
@@ -152,7 +152,9 @@ gulp.task('uglify', function () {
   return gulp.src(assets)
     .pipe(plugins.ngAnnotate())
     .pipe(plugins.uglify({
-      mangle: true
+      mangle: false
+    }).on('error', function(err) {
+      console.log('Uglify error : ', err.toString());
     }))
     .pipe(plugins.concat('application.min.js'))
     .pipe(plugins.rev())
