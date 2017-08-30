52d267ecbb0d26e26031d39478f10418559581e8
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2017-08-30 14:47:19 +0200

    clean ident

--- gulpfile.js
+++ gulpfile.js
@@ -153,7 +153,7 @@ gulp.task('uglify', function () {
     .pipe(plugins.ngAnnotate())
     .pipe(plugins.uglify({
       mangle: true
-    }).on('error', function(err) {
+    }).on('error', function (err) {
       console.log('Uglify error : ', err.toString());
     }))
     .pipe(plugins.concat('application.min.js'))
