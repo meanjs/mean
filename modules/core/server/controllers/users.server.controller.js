b81847a6330ab28907b46e860c04e8f633200831
Mark Davis <markd315@gmail.com>
2017-09-29 17:24:15 -0400

    Made tentative changes to users.server.controller

--- modules/core/server/controllers/users.server.controller.js
+++ modules/core/server/controllers/users.server.controller.js
@@ -1,7 +1,7 @@
 /* Dependencies */
 var mongoose = require('mongoose'), 
-    User = require('../models/users.server.model.js');
-    //TODO define user schema
+    User = require('..../users/server/models/users.server.model.js');
+    //TODO redefine user schema
 /*
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update users.
   On an error you should send a 404 status code, as well as the error message. 
@@ -15,14 +15,6 @@ exports.create = function(req, res) {
 
   /* Instantiate a User */
   var user = new User(req.body);
-
-  /* save the coordinates (located in req.results if there is an address property) */
-  if(req.results) {
-    user.coordinates = {
-      latitude: req.results.lat, 
-      longitude: req.results.lng
-    };
-  }
 
   /* Then save the user */
   user.save(function(err) {
@@ -46,17 +38,10 @@ exports.update = function(req, res) {
   var user = req.user;
 
   /* Replace the article's properties with the new properties found in req.body */
+  //TODO replace whatever attributes we will need from this request.
   user.name = req.body.name;
   user.code = req.body.code;
   user.address = req.body.address;
-
-  /* save the coordinates (located in req.results if there is an address property) */
-  if(req.results) {
-    user.coordinates = {
-      latitude: req.results.lat, 
-      longitude: req.results.lng
-    };
-  }
 
   /* Save the article */
   user.save(function(err) {
@@ -86,6 +71,7 @@ exports.delete = function(req, res) {
 
 /* Retreive all the directory users, sorted alphabetically by user code */
 exports.list = function(req, res) {
+  //TODO find a new field to sort users by.
   User.find().sort('code').exec(function(err, users) {
     if(err) {
       res.status(400).send(err);
