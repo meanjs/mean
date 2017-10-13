487823ef287d56eb1692c2b546ef89288bec1329
Mark Davis <markd315@gmail.com>
2017-10-13 18:37:41 -0400

    Changes to test config

--- config/env/test.js
+++ config/env/test.js
@@ -92,7 +92,7 @@ module.exports = {
           email: 'admin@localhost.com',
           firstName: 'Admin',
           lastName: 'Local',
-          roles: ['admin', 'user']
+          roles: ['admin']
         }
       }, {
         overwrite: true,
@@ -101,7 +101,7 @@ module.exports = {
           email: 'user@localhost.com',
           firstName: 'User',
           lastName: 'Local',
-          roles: ['user']
+          roles: ['ta']
         }
       }]
     }, {
