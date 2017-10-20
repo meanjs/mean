185192e8f24efee36522427e80a8bf605766e513
Zach <zmurta@ufl.edu>
2017-10-20 16:26:19 -0400

    slider bar

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -15,7 +15,14 @@
 		  alert('hello');
     }
     
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value; // Display the default slider value
     
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
   
 
 
