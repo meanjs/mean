f049f5d42cf83621d850c13fd3008d8e8b9be50b
Zach <zmurta@ufl.edu>
2017-10-20 18:54:12 -0400

    js changes

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -23,7 +23,15 @@
     slider.oninput = function() {
         output.innerHTML = this.value;
     }
-  
+
+    $(document).ready(function(){
+    $(".dropdown").on("hide.bs.dropdown", function(){
+      $(".btn").html('Dropdown <span class="caret"></span>');
+    });
+    $(".dropdown").on("show.bs.dropdown", function(){
+      $(".btn").html('Dropdown <span class="caret caret-up"></span>');
+    });
+  });
 
 
 
