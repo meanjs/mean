56e54771b4a96a5729177ab774f42bd249f682fa
Zach <zmurta@ufl.edu>
2017-10-20 16:45:41 -0400

    finished slider

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -15,10 +15,10 @@
 		  alert('hello');
     }
     
+    //CALORIE SLIDER
     var slider = document.getElementById("calories");
-    var output = document.getElementById("demo");
+    var output = document.getElementById("calVal");
     output.innerHTML = slider.value; // Display the default slider value
-    
     // Update the current slider value (each time you drag the slider handle)
     slider.oninput = function() {
         output.innerHTML = this.value;
