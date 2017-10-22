3f4901571fe8245bb347de50b24906888dc35f47
Zach <zmurta@ufl.edu>
2017-10-22 16:48:35 -0400

    filter

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -44,10 +44,12 @@
       $scope.filterSelection = function(c) {
         var x, i;
         x = document.getElementsByClassName("filterDiv");
-        if (c == "all") c = "";
+        if (c == "all")
+          c = "";
         for (i = 0; i < x.length; i++) {
           w3RemoveClass(x[i], "show");
-          if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+          if (x[i].className.indexOf(c) > -1)
+            w3AddClass(x[i], "show");
         }
       }
       
