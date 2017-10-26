2629320653ccbf92cff9ba26db1354680c9ea35e
Mark Davis <markd315@gmail.com>
2017-10-25 21:57:31 -0400

    Replayed mongolab changed onto catsAndMods

---
+++ modules/articles/server/models/module.server.model.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  path = require('path'),
+  config = require(path.resolve('./config/config')),
+  chalk = require('chalk');
+
+/**
+ * Module Schema
+ */
+var ModuleSchema = new Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  title: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Title cannot be blank'
+  }
+});
+
+ModuleSchema.statics.seed = seed;
+
+mongoose.model('Module', ModuleSchema);
