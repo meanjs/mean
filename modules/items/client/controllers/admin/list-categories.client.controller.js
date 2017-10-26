c075ed14474dc9ddceb00a52ac85ce92178f9b94
kevdaci <kevdaci@gmail.com>
2017-10-26 17:48:12 -0400

    Add the views for the add/delete cats and mods

--- modules/items/client/controllers/admin/list-categories.client.controller.js
+++ modules/items/client/controllers/admin/list-categories.client.controller.js
@@ -10,5 +10,24 @@
   function ItemsAdminCategoriesController(ItemsService) {
     var vm = this;
 
+    vm.categories= [
+    	{
+    		name: "Category 1"
+    	},
+    	{
+    		name: "Category 2"
+    	},
+    	{
+    		name: "Cateogry 3"
+    	},
+    	{
+    		name: "Category 4"
+    	}
+    ];
+
+    vm.deleteCategory = function(category){
+    	vm.categories.splice(vm.categories.indexOf(category), 1);
+    }
+
   }
 }());
--- modules/items/client/controllers/admin/list-modules.client.controller.js
+++ modules/items/client/controllers/admin/list-modules.client.controller.js
@@ -10,5 +10,24 @@
   function ItemsAdminModulesController(ItemsService) {
     var vm = this;
 
+    vm.modules = [
+    	{
+    		name: "Module 1"
+    	},
+    	{
+    		name: "Module 2"
+    	},
+    	{
+    		name: "Module 3"
+    	},
+    	{
+    		name: "Module 4"
+    	}
+    ];
+
+    vm.deleteModule = function(module){
+    	vm.modules.splice(vm.modules.indexOf(module), 1);
+    }
+
   }
 }());
