1de5a5edcb12c360bf0829cb4232ff049e1e065f
Bryan Rivera <brbriv@gmail.com>
2017-10-25 21:46:32 -0400

    Added the route file

--- modules/users/server/routes/catalog.server.routes.js
+++ modules/users/server/routes/catalog.server.routes.js
@@ -1,3 +1,11 @@
 'use strict';
 
 module.exports = function (app) {
+  var users = require('../controllers/catalog.server.controller');
+
+  app.route('/catalog').get(users.list)
+  .put(users.update)
+  .delete(users.delete);
+
+  app.param('userId', users.userById);
+};
