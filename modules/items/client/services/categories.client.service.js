e488f3008d35b6d157ce672917bb9cfde4e76882
Mark Davis <markd315@gmail.com>
2017-10-27 13:22:23 -0400

    Created services

---
+++ modules/items/client/services/categories.client.service.js
@@ -0,0 +1,43 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('items.services')
+    .factory('CategoriesService', CategoriesService);
+
+  ItemsService.$inject = ['$resource', '$log'];
+
+  function CategoriesService($resource, $log) {
+    var Category = $resource('/api/categories', {}, 
+    {
+      list: {
+        method: 'GET'
+      }
+    },
+    {
+      new:{
+        method: 'POST'
+      }
+    },
+    {
+      rem:{
+        method: 'DELETE'
+      }
+    }
+    });
+
+    angular.extend(Category, {
+      create: function (cat) {
+        return this.new(cat).$promise;
+      },
+      delete: function (cat) {
+        return this.del(cat).$promise;
+      },
+      getAll: function () {
+        return this.list().$promise;
+      }
+    });
+
+    return Category;
+  }
+}());
---
+++ modules/items/client/services/modules.client.service.js
@@ -0,0 +1,43 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('items.services')
+    .factory('ModulesService', ModulesService);
+
+  ItemsService.$inject = ['$resource', '$log'];
+
+  function ModulesService($resource, $log) {
+    var Module = $resource('/api/modules', {}, 
+    {
+      list: {
+        method: 'GET'
+      }
+    },
+    {
+      new:{
+        method: 'POST'
+      }
+    },
+    {
+      rem:{
+        method: 'DELETE'
+      }
+    }
+    });
+
+    angular.extend(Module, {
+      create: function (mod) {
+        return this.new(mod).$promise;
+      },
+      delete: function (mod) {
+        return this.del(mod).$promise;
+      },
+      getAll: function () {
+        return this.list().$promise;
+      }
+    });
+
+    return Module;
+  }
+}());
