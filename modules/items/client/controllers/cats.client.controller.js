b89635ce64daa36f699480008fa146cad00700e7
Cianan_Roden-Corrent <crodencorrent@ufl.edu>
2017-10-30 10:04:41 -0400

    CHanged form-category view for adding categories to reference cats rather than items. Began to flesh out the controller.

--- modules/items/client/controllers/cats.client.controller.js
+++ modules/items/client/controllers/cats.client.controller.js
@@ -8,7 +8,13 @@
   CatsController.$inject = ['$scope', '$state', '$window', 'catResolve', 'Authentication', 'Notification'];
 
   function CatsController($scope, $state, $window, cat, Authentication, Notification) {
-    
+    var vm = this;
+
+    vm.cat = cat;
+    vm.authentication = Authentication;
+    vm.form = {};
+    vm.remove = remove;
+    vm.save = save;
 
   }
 }());
