6e84e04ee3f250611644715745d3d5dd4206b0a4
Cianan_Roden-Corrent <crodencorrent@ufl.edu>
2017-10-30 10:13:20 -0400

    Added some methods to controller, change some more itmes mentions to cats

--- modules/items/client/controllers/cats.client.controller.js
+++ modules/items/client/controllers/cats.client.controller.js
@@ -15,6 +15,36 @@
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
+    function remove() {
+      if ($window.confirm('Are you sure you want to delete?')) {
+        vm.cat.$remove(function () {
+          $state.go('admin.items.categories');
+          Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Category deleted successfully!' });
+        });
+      }
+    }
 
+    // Save Item
+    function save(isValid) {
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.form.catForm');
+        return false;
+      }
+
+      // Create a new item, or update the current instance
+      vm.cat.createOrUpdate()
+        .then(successCallback)
+        .catch(errorCallback);
+
+      function successCallback(res) {
+        $state.go('admin.items.categories'); // should we send the User to the list or the updated Item's view?
+        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Category saved successfully!' });
+      }
+
+      function errorCallback(res) {
+        Notification.error({ message: res.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Category save error!' });
+      }
+    }
   }
 }());
+
