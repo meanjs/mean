5a76ea8b502d2959eda275c53fe2fb8a0a2e27e7
Cianan_Roden-Corrent <crodencorrent@ufl.edu>
2017-10-30 18:53:44 -0400

    almost got delete working on categories, it doesn't throw any errors and succesfully trasmits a delete message, but isn't delete the category

--- modules/items/client/controllers/admin/list-categories.client.controller.js
+++ modules/items/client/controllers/admin/list-categories.client.controller.js
@@ -5,15 +5,20 @@
     .module('items.admin')
     .controller('ItemsAdminCategoriesController', ItemsAdminCategoriesController);
 
-  ItemsAdminCategoriesController.$inject = ['ItemsService', 'CategoriesService'];
+  ItemsAdminCategoriesController.$inject = ['ItemsService', 'CategoriesService', '$window', '$state', '$scope', 'Notification'];
 
-  function ItemsAdminCategoriesController(ItemsService, CategoriesService) {
+  function ItemsAdminCategoriesController(ItemsService, CategoriesService, $window, $state, $scope, Notification) {
     var vm = this;
 
-    vm.categories= CategoriesService.query()
-
-    vm.deleteCategory = function(category){
-    	vm.categories.splice(vm.categories.indexOf(category), 1);
+    vm.categories= CategoriesService.query();
+    vm.remove= remove;
+    function remove(cat) {
+      if ($window.confirm('Are you sure you want to create?')) {
+        cat.$rem(function () {
+          $state.go('items.list');
+          Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Item created successfully!' });
+        });
+      }
     }
 
   }
