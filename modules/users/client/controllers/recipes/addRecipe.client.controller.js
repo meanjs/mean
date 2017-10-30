4acd407720c3990e1a8ff48c156714708801b33b
“Cristo <cristo.silva@yahoo.com>
2017-10-30 13:15:55 -0400

    added cookingStyle, changed addRecipe

--- modules/users/client/controllers/recipes/addRecipe.client.controller.js
+++ modules/users/client/controllers/recipes/addRecipe.client.controller.js
@@ -5,10 +5,35 @@
     .module('users')
     .controller('AddRecipeController', AddRecipeController);
 
-  AddRecipeController.$inject = ['UsersService', '$scope', '$http'];
+  AddRecipeController.$inject = ['UsersService', '$scope', '$http','Authentication','Notification'];
 
-  function AddRecipeController(UsersService, $scope, $http) {
+  function AddRecipeController(UsersService, $scope, $http,Authentication,Notification) {
     var vm = this;
+
+    vm.user = Authentication.user;
+    vm.updateUserProfile = updateUserProfile;
+
+    function updateUserProfile(isValid) {
+
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.userForm');
+
+        return false;
+      }
+
+      var user = new UsersService(vm.user);
+
+      user.$update(function (response) {
+        $scope.$broadcast('show-errors-reset', 'vm.userForm');
+
+        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Edit profile successful!' });
+        Authentication.user = response;
+      }, function (response) {
+        Notification.error({ message: response.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Edit profile failed!' });
+      });
+    }
+
+
 
     var alternative = {
       'hello' : 'world'
