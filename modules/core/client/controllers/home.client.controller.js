a9f0b5990c0e0caccf3723c42196d8a5ca17ae50
Himal <himpatel@ufl.edu>
2017-10-30 21:25:00 -0400

    User can add a specific recipe by clicking + button in each picture

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,10 +5,13 @@
     .module('core')
     .controller('HomeController', HomeController);
 
-  HomeController.$inject = ['$scope'];
+  HomeController.$inject = ['$scope', 'Authentication', 'Notification'];
 
-  function HomeController($scope) {
+  function HomeController($scope, Authentication, Notification) {
     var vm = this;
+
+    vm.user = Authentication.user;
+    vm.updateUserProfile = updateUserProfile;
 
     //DO YOUR FRONTEND JS CODE HERE
     // $scope.alert = () => {
@@ -24,6 +27,25 @@
         output.innerHTML = this.value;
     }
 
+     function updateUserProfile(isValid) {
+
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.userForm');
+
+        return false;
+      }
+
+      var user = new UsersService(vm.user);
+
+      user.$update(function (response) {
+        $scope.$broadcast('show-errors-reset', 'vm.userForm');
+
+        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Edit profile successful!' });
+        Authentication.user = response;
+      }, function (response) {
+        Notification.error({ message: response.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Edit profile failed!' });
+      });
+    }
     /*
     $scope.ready = function() {
       var newSelect=document.createElement('select');
