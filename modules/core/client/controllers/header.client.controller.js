94c53c2767545712149f5e8d594d4a494091dcd8
jmcacioppo <jmcacioppo0531@gmail.com>
2017-10-30 22:04:22 -0400

    Trying to get TransferService to work on search bar

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -5,9 +5,9 @@
     .module('core')
     .controller('HeaderController', HeaderController);
 
-  HeaderController.$inject = ['$scope', '$state', 'Authentication', 'menuService'];
+  HeaderController.$inject = ['$scope', '$state', 'Authentication', 'menuService', 'TransferService', '$http'];
 
-  function HeaderController($scope, $state, Authentication, menuService) {
+  function HeaderController($scope, $state, Authentication, menuService, TransferService, $http) {
     var vm = this;
 
     vm.accountMenu = menuService.getMenu('account').items[0];
@@ -20,6 +20,56 @@
     function stateChangeSuccess() {
       // Collapsing the menu after navigation
       vm.isCollapsed = false;
+    }
+
+    // GET ALTERNATIVES FROM RECIPE
+    $scope.map = [];
+		$scope.in_food_group;
+		$scope.orig_nutrient_amount;
+		$scope.all_alt_in_group = [];
+		$scope.have_match = 0;
+		$scope.c_method = "baked";
+    $scope.search = "butter";
+
+    vm.getAlternatives = getAlternatives;
+
+    function getAlternatives() {
+      $scope.map = [];
+      $scope.orig_nutrient_amount = 0;
+
+      $http.get('./modules/users/client/controllers/recipes/food_alternatives.json')
+        .then( (response) => {
+          response.data.cooking_methods.forEach( (cooking_method, i) => {
+						cooking_method.food_groups.forEach( (food_group, j) => {
+							food_group.food_alts.forEach( (food_alt, k) => {
+
+								if((food_alt.db_name == $scope.search) && ($scope.c_method == cooking_method.method_name)){
+									$scope.have_match = 1;
+									$scope.orig_ndbno = food_alt.db_ndbno;
+									$scope.in_food_group = food_group.group_name;
+									$scope.orig_nutrient_amount = food_alt.db_main_nutrient.db_amount;
+								}
+								else{
+									$scope.all_alt_in_group.push(food_alt);
+								}
+							});
+							if($scope.have_match == 1){
+								$scope.all_alt_in_group.forEach((alt_item, i) => {
+									if(alt_item.db_main_nutrient.db_amount < $scope.orig_nutrient_amount){
+										$scope.map.push({"map_ndbno": alt_item.db_ndbno, "map_name": alt_item.db_name});
+									}
+								});
+							}
+							$scope.have_match = 0;
+							$scope.all_alt_in_group = [];
+						});
+					});
+        });
+
+        TransferService.setAlternatives($scope.map);
+        TransferService.setIngredient($scope.searchFood);
+
+        $state.go('search');
     }
   }
 }());
--- modules/core/client/controllers/search.client.controller.js
+++ modules/core/client/controllers/search.client.controller.js
@@ -5,9 +5,9 @@
     .module('core')
     .controller('SearchController', SearchController);
 
-  SearchController.$inject = ['$scope'];
+  SearchController.$inject = ['$scope', 'menuService', 'TransferService'];
 
-  function SearchController($scope) {
+  function SearchController($scope, menuService, TransferService) {
     $scope.pages = [{name:"Whole Milk", calories:"105", protein:"8", sugar:"13", fat:"2.5"},
                     {name:"Comparison View", show: false}
     ]
@@ -53,6 +53,46 @@
       $scope.flipped = !$scope.flipped;
     }
 
+    $scope.alternatives = TransferService.getAlternatives();
+    //$scope.ingredient = TransferService.getIngredient();
+    console.log("Here are alternatives: ", $scope.alternatives);
+    //console.log("Here is the ingredient", $scope.ingredient);
+
+    // API KEY
+		var apiKey = 'YAJ2M9l67OaqNMPCEfBcoccVtQDY5LPUR20rFzP8';
+
+		// FOR REPORT
+		var type = "b";
+		var format = "json";
+
+		// FOR INDIVIDUAL SEARCHES
+		var sort = "n";
+		var max = "200";
+		var ds = 'Standard Reference';
+
+    $scope.getReport = (searchedItem) => {
+			var reportURL = 
+			  	"http://api.nal.usda.gov/ndb/reports/" + 
+			  	"?ndbno=" + searchedItem + 
+          "&type=" + type + 
+          "&format=" + format + 
+          "&api_key=" + apiKey; 
+
+        getURL(reportURL)
+          .then( (results) => {
+            $scope.searched = results.data;
+            assignFood();
+          });
+		}
+
+    function assignFood() {
+			$scope.food = $scope.searched.report.food.name.toLowerCase();
+			$scope.nutrients = $scope.searched.report.food.nutrients;
+		}
+
+    function getURL(url) {
+			return $http.get(url);
+		}
 
   }
 }());
