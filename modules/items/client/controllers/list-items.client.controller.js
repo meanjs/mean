fdeee6c0724bad09f4940f4d4e8909f3933ebb52
Mark Davis <markd315@gmail.com>
2017-11-09 14:59:22 -0500

    Removed some form grouping on items/create to fix the indentation

--- modules/items/client/controllers/list-items.client.controller.js
+++ modules/items/client/controllers/list-items.client.controller.js
@@ -5,11 +5,13 @@
     .module('items')
     .controller('ItemsListController', ItemsListController);
 
-  ItemsListController.$inject = ['ItemsService'];
+  ItemsListController.$inject = ['ItemsService', 'CategoriesService', 'ModulesService'];
 
-  function ItemsListController(ItemsService) {
+  function ItemsListController(ItemsService, CategoriesService, ModulesService) {
     var vm = this;
 
     vm.items = ItemsService.query();
+    vm.categories = CategoriesService.query();
+  	vm.modules = ModulesService.query();
   }
 }());
