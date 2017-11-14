656646614eab6742de7fd4b8fc001fde0bc777fd
Cianan_Roden-Corrent <crodencorrent@ufl.edu>
2017-11-13 19:44:51 -0500

    Sucessfully added a single meaningful test to the approve applicants view, confirming that it can indeed perform a successful get request on a list of users.

--- modules/users/tests/client/admin.view.applicants.controller.tests.js
+++ modules/users/tests/client/admin.view.applicants.controller.tests.js
@@ -1,15 +1,15 @@
 (function () {
   'use strict';
 
-  describe('Admin Items List Controller Tests', function () {
+  describe('HOOO BOY THIS IS WHERE THE ADMIN VIEW CONTROLLER TESTS GO MY FRIEND', function () {
     // Initialize global variables
-    var ItemsAdminListController,
+    var ViewApplicantsController,
       $scope,
       $httpBackend,
       $state,
       Authentication,
-      ItemsService,
-      mockItem;
+      ApplicantsService,
+      mockUser;
 
     // The $resource service augments the response object with methods for updating and deleting the resource.
     // If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
@@ -36,7 +36,7 @@
     // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
     // This allows us to inject a service but then attach it to a variable
     // with the same name as the service.
-    beforeEach(inject(function ($controller, $rootScope, _$state_, _$httpBackend_, _Authentication_, _ItemsService_) {
+    beforeEach(inject(function ($controller, $rootScope, _$state_, _$httpBackend_, _Authentication_, _ApplicantsService_) {
       // Set a new global scope
       $scope = $rootScope.$new();
 
@@ -44,30 +44,26 @@
       $httpBackend = _$httpBackend_;
       $state = _$state_;
       Authentication = _Authentication_;
-      ItemsService = _ItemsService_;
+      ApplicantsService = _ApplicantsService_;
 
       // Ignore parent template get on state transitions
-      $httpBackend.whenGET('/modules/items/client/views/list-items.client.view.html').respond(200, '');
+      $httpBackend.whenGET('/modules/users/client/views/admin/view-applicants.html').respond(200, '');
       $httpBackend.whenGET('/modules/core/client/views/home.client.view.html').respond(200, '');
-      $httpBackend.whenGET('/api/categories').respond(200, '');
-      $httpBackend.whenGET('/api/modules').respond(200, '');
-
 
       // create mock item
-      mockItem = new ItemsService({
-        _id: '525a8422f6d0f87f0e407a33',
-        title: 'An Item about MEAN',
-        content: 'MEAN rocks!'
+      mockUser = new ApplicantsService({
+        roles: ['ta'],
+        approvedStatus: true
       });
 
       // Mock logged in user
       Authentication.user = {
-        roles: ['ta'],
+        roles: ['admin'],
         approvedStatus: true
       };
 
       // Initialize the Items List controller.
-      ItemsAdminListController = $controller('ItemsAdminListController as vm', {
+      ViewApplicantsController = $controller('ViewApplicantsController as vm', {
         $scope: $scope
       });
 
@@ -76,23 +72,23 @@
     }));
 
     describe('Instantiate', function () {
-      var mockItemList;
+      var mockUserList;
 
       beforeEach(function () {
-        mockItemList = [mockItem, mockItem];
+        mockUserList = [mockUser, mockUser];
       });
 
-      it('should send a GET request and return all items', inject(function (ItemsService) {
+      it('should send a GET request and return all users', inject(function (ItemsService) {
         // Set POST response
-        $httpBackend.expectGET('/api/items').respond(mockItemList);
+        $httpBackend.expectGET('/api/unapproved').respond(mockUserList);
 
 
         $httpBackend.flush();
 
         // Test form inputs are reset
-        expect($scope.vm.items.length).toEqual(2);
-        expect($scope.vm.items[0]).toEqual(mockItem);
-        expect($scope.vm.items[1]).toEqual(mockItem);
+        expect($scope.vm.unapprovedUsers.length).toEqual(2);
+        expect($scope.vm.unapprovedUsers[0]).toEqual(mockUser);
+        expect($scope.vm.unapprovedUsers[1]).toEqual(mockUser);
 
       }));
     });
