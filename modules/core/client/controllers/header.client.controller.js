d850d871a7bb66dfee1b83b37f1cb3ccd72052ff
Himal <himpatel@ufl.edu>
2017-11-14 14:07:06 -0500

    resolved errors in header controller

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -24,11 +24,11 @@
 
     // GET ALTERNATIVES FROM RECIPE
     $scope.map = [];
-		$scope.in_food_group;
-		$scope.orig_nutrient_amount;
-		$scope.all_alt_in_group = [];
-		$scope.have_match = 0;
-		$scope.c_method = "baked";
+    $scope.in_food_group;
+    $scope.orig_nutrient_amount;
+    $scope.all_alt_in_group = [];
+    $scope.have_match = 0;
+    $scope.c_method = 'baked';
 
     vm.getAlternatives = getAlternatives;
 
@@ -39,38 +39,37 @@
       $scope.map.push(vm.searchFood);
 
       $http.get('./modules/users/client/controllers/recipes/food_alternatives.json')
-        .then( (response) => {
-          response.data.cooking_methods.forEach( (cooking_method, i) => {
-						cooking_method.food_groups.forEach( (food_group, j) => {
-							food_group.food_alts.forEach( (food_alt, k) => {
+        .then((response) => {
+          response.data.cooking_methods.forEach((cooking_method, i) => {
+            cooking_method.food_groups.forEach((food_group, j) => {
+              food_group.food_alts.forEach((food_alt, k) => {
 
-								if((food_alt.db_name == vm.searchFood) && ($scope.c_method == cooking_method.method_name)){
-									$scope.have_match = 1;
-									$scope.orig_ndbno = food_alt.db_ndbno;
-									$scope.in_food_group = food_group.group_name;
-									$scope.orig_nutrient_amount = food_alt.db_main_nutrient.db_amount;
-								}
-								else{
-									$scope.all_alt_in_group.push(food_alt);
-								}
-							});
-							if($scope.have_match == 1){
-								$scope.all_alt_in_group.forEach((alt_item, i) => {
-									if(alt_item.db_main_nutrient.db_amount < $scope.orig_nutrient_amount){
-										$scope.map.push({"map_ndbno": alt_item.db_ndbno, "map_name": alt_item.db_name});
-									}
-								});
-							}
-							$scope.have_match = 0;
-							$scope.all_alt_in_group = [];
-						});
-					});
+                if ((food_alt.db_name === vm.searchFood) && ($scope.c_method === cooking_method.method_name)) {
+                  $scope.have_match = 1;
+                  $scope.orig_ndbno = food_alt.db_ndbno;
+                  $scope.in_food_group = food_group.group_name;
+                  $scope.orig_nutrient_amount = food_alt.db_main_nutrient.db_amount;
+                } else {
+                  $scope.all_alt_in_group.push(food_alt);
+                }
+              });
+              if ($scope.have_match === 1) {
+                $scope.all_alt_in_group.forEach((alt_item, i) => {
+                  if (alt_item.db_main_nutrient.db_amount < $scope.orig_nutrient_amount) {
+                    $scope.map.push({ 'map_ndbno': alt_item.db_ndbno, 'map_name': alt_item.db_name });
+                  }
+                });
+              }
+              $scope.have_match = 0;
+              $scope.all_alt_in_group = [];
+            });
+          });
         });
 
-        // FIRST ITEM IN MAP IS THE ORIGINAL ITEM
-        TransferService.setAlternatives($scope.map);
+      // FIRST ITEM IN MAP IS THE ORIGINAL ITEM
+      TransferService.setAlternatives($scope.map);
 
-        $state.go('search');
+      $state.go('search');
     }
   }
 }());
