f3718a501e24b25eb0d675e35def67d63e018637
Himal <himpatel@ufl.edu>
2017-11-15 00:22:13 -0500

    Nothing

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -12,49 +12,48 @@
 
     vm.authentication = Authentication;
 
-    //CALORIE SLIDER
-    var slider = document.getElementById("calories");
-    var output = document.getElementById("calVal");
+    // CALORIE SLIDER
+    var slider = document.getElementById('calories');
+    var output = document.getElementById('calVal');
     output.innerHTML = slider.value; // Display the default slider value
 
     // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() {
-        output.innerHTML = this.value;
-    }
+    slider.oninput = function () {
+      output.innerHTML = this.value;
+    };
 
     // Showing community or my recipes
     $scope.showCommunity = true;
 
     $scope.showPopular = () => {
       $scope.showCommunity = true;
-    }
+    };
 
     $scope.showMyRecipes = () => {
       $scope.showCommunity = false;
-    }
+    };
 
     // GET COMMUNITY RECIPES
     CommunityService.getList()
       .then(CommunityRecipeSuccess)
-      .catch(failure)
+      .catch(failure);
 
     function CommunityRecipeSuccess(response) {
-      console.log("Community recipes success: ", response);
+      console.log('Community recipes success: ', response);
       $scope.communityRecipes = response;
     }  
-    
     // GET MY RECIPES
     CommunityService.getMyRecipes()
       .then(MyRecipeSuccess)
-      .catch(failure)
+      .catch(failure);
 
     function MyRecipeSuccess(response) {
-      console.log("My recipes success: ", response.recipes);
+      console.log('My recipes success: ', response.recipes);
       $scope.myRecipes = response.recipes;
     }
 
     function failure(error) {
-      console.log("Failure: ", error);
+      console.log('Failure: ', error);
     }  
   }
 }());
