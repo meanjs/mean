c70c76b93d07270b81c5194e75904a04e30bccff
Brian <bmbenda@ufl.edu>
2017-11-22 12:05:45 -0500

    Logo in header redirects to appropriate user dashboard

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -15,6 +15,17 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
       $scope.isCollapsed = !$scope.isCollapsed;
     };
 
+    $scope.logoRedirect = function(){
+      if ($scope.authentication.user.roles.indexOf('Organization') >= 0) {
+          $state.go('orgDash.eventList');
+          console.log($state.current);
+        }
+        if ($scope.authentication.user.roles.indexOf('Business') >= 0) {
+          $state.go('bizDash.eventList');
+          console.log($state.current);
+        }
+    }
+
     // Collapsing the menu after navigation
     $scope.$on('$stateChangeSuccess', function () {
       $scope.isCollapsed = false;
