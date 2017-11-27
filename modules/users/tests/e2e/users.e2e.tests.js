85b88a9a705c24dae173781390dd444339c30dcc
Cianan_Roden-Corrent <crodencorrent@ufl.edu>
2017-11-27 15:05:18 -0500

    readded password validation E2E tests

--- modules/users/tests/e2e/users.e2e.tests.js
+++ modules/users/tests/e2e/users.e2e.tests.js
@@ -771,6 +771,117 @@ describe('Users E2E Tests:', function () {
       element(by.css('button[type="submit"]')).click();
       expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/signin');
     });
-
   });
-});
+    describe('Change Password Settings Validation', function () {
+    it('Should report missing passwords', function () {
+      signin();
+      browser.get('http://localhost:3001/settings/password');
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Errors
+      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('Your current password is required.');
+      expect(element.all(by.css('.error-text')).get(1).getText()).toBe('Enter a new password.');
+      expect(element.all(by.css('.error-text')).get(2).getText()).toBe('Verify your new password.');
+    });
+
+    it('Should report a password with less than 10 characters long - "P@$$w0rd!"', function () {
+      browser.get('http://localhost:3001/settings/password');
+      // Enter Current Password
+      element(by.model('vm.passwordDetails.currentPassword')).sendKeys(admin.password);
+      // Enter Invalid Password
+      element(by.model('vm.passwordDetails.newPassword')).sendKeys('P@$$w0rd!');
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Error
+      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must be at least 10 characters long.');
+    });
+
+    it('Should report a password with greater than 128 characters long.', function () {
+      browser.get('http://localhost:3001/settings/password');
+      // Enter Current Password
+      element(by.model('vm.passwordDetails.currentPassword')).sendKeys(admin.password);
+      // Enter Invalid Password
+      element(by.model('vm.passwordDetails.newPassword')).sendKeys(')!/uLT="lh&:`6X!]|15o!$!TJf,.13l?vG].-j],lFPe/QhwN#{Z<[*1nX@n1^?WW-%_.*D)m$toB+N7z}kcN#B_d(f41h%w@0F!]igtSQ1gl~6sEV&r~}~1ub>If1c+');
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Error
+      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must be fewer than 128 characters.');
+    });
+
+    it('Should report a password with more than 3 or more repeating characters - "P@$$w0rd!!!"', function () {
+      browser.get('http://localhost:3001/settings/password');
+      // Enter Current Password
+      element(by.model('vm.passwordDetails.currentPassword')).sendKeys(admin.password);
+      // Enter Invalid Password
+      element(by.model('vm.passwordDetails.newPassword')).sendKeys('P@$$w0rd!!!');
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Error
+      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password may not contain sequences of three or more repeated characters.');
+    });
+
+    it('Should report a password with no uppercase letters - "p@$$w0rd!!"', function () {
+      browser.get('http://localhost:3001/settings/password');
+      // Enter Current Password
+      element(by.model('vm.passwordDetails.currentPassword')).sendKeys(admin.password);
+      // Enter Invalid Password
+      element(by.model('vm.passwordDetails.newPassword')).sendKeys('p@$$w0rd!!');
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Error
+      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must contain at least one uppercase letter.');
+    });
+
+    it('Should report a password with less than one number - "P@$$word!!"', function () {
+      browser.get('http://localhost:3001/settings/password');
+      // Enter Current Password
+      element(by.model('vm.passwordDetails.currentPassword')).sendKeys(admin.password);
+      // Enter Invalid Password
+      element(by.model('vm.passwordDetails.newPassword')).sendKeys('P@$$word!!');
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Error
+      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must contain at least one number.');
+    });
+
+    it('Should report a password with less than one special character - "Passw0rdss"', function () {
+      browser.get('http://localhost:3001/settings/password');
+      // Enter Current Password
+      element(by.model('vm.passwordDetails.currentPassword')).sendKeys(admin.password);
+      // Enter Invalid Password
+      element(by.model('vm.passwordDetails.newPassword')).sendKeys('Passw0rdss');
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Error
+      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('The password must contain at least one special character.');
+    });
+
+    it('Should report passwords do not match', function () {
+      browser.get('http://localhost:3001/settings/password');
+      // Enter Current Password
+      element(by.model('vm.passwordDetails.currentPassword')).sendKeys(admin.password);
+      // Enter New Password
+      element(by.model('vm.passwordDetails.newPassword')).sendKeys('P@$$w0rds!!');
+      // Verify New Password
+      element(by.model('vm.passwordDetails.verifyPassword')).sendKeys(admin.password);
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Errors
+      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('Passwords do not match.');
+    });
+
+    it('Should change the password to - "P@$$w0rds!!"', function () {
+      browser.get('http://localhost:3001/settings/password');
+      // Enter Current Password
+      element(by.model('vm.passwordDetails.currentPassword')).sendKeys(admin.password);
+      // Enter New Password
+      element(by.model('vm.passwordDetails.newPassword')).sendKeys('P@$$w0rds!!');
+      // Verify New Password
+      element(by.model('vm.passwordDetails.verifyPassword')).sendKeys('P@$$w0rds!!');
+      // Click Submit button
+      element(by.css('button[type=submit]')).click();
+      // Password Changed
+      expect(element.all(by.css('.ui-notification')).get(0).getText()).toBe('Password Changed Successfully');
+    });
+  });
+  });
