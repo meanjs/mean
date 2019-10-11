a8b9f27e78ea562be86a92b30f497e4ccde7ad94
Ilan Biala <ilan.biala@gmail.com>
2019-10-10 23:22:22 -0400

    Update auth-interceptor.client.service.js

--- modules/core/client/services/interceptors/auth-interceptor.client.service.js
+++ modules/core/client/services/interceptors/auth-interceptor.client.service.js
@@ -31,7 +31,7 @@
           case 404:
             $injector.get('$state').go('not-found', { message: rejection.data.message });
             break;
-          case -1:  // Handle error if no response from server(Network Lost or Server not responding)
+          case -1: // Handle error if no response from server(Network Lost or Server not responding)
             var Notification = $injector.get('Notification');
             Notification.error({ message: 'No response received from server. Please try again later.', title: 'Error processing request!', delay: 5000 });
             break;
