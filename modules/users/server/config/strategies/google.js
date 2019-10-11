a9128c25089a4e74464f87015fb1bb49b6a6f03d
Ilan Biala <ilan.biala@gmail.com>
2019-10-10 23:21:56 -0400

    Update google.js

--- modules/users/server/config/strategies/google.js
+++ modules/users/server/config/strategies/google.js
@@ -18,26 +18,26 @@ module.exports = function (config) {
       'https://www.googleapis.com/auth/userinfo.email'
     ]
   },
-    function (req, accessToken, refreshToken, profile, done) {
-      // Set the provider data and include tokens
-      var providerData = profile._json;
-      providerData.accessToken = accessToken;
-      providerData.refreshToken = refreshToken;
+  function (req, accessToken, refreshToken, profile, done) {
+    // Set the provider data and include tokens
+    var providerData = profile._json;
+    providerData.accessToken = accessToken;
+    providerData.refreshToken = refreshToken;
 
-      // Create the user OAuth profile
-      var providerUserProfile = {
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        displayName: profile.displayName,
-        email: profile.emails[0].value,
-        username: profile.username,
-        profileImageURL: (providerData.picture) ? providerData.picture : undefined,
-        provider: 'google',
-        providerIdentifierField: 'id',
-        providerData: providerData
-      };
+    // Create the user OAuth profile
+    var providerUserProfile = {
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      displayName: profile.displayName,
+      email: profile.emails[0].value,
+      username: profile.username,
+      profileImageURL: (providerData.picture) ? providerData.picture : undefined,
+      provider: 'google',
+      providerIdentifierField: 'id',
+      providerData: providerData
+    };
 
-      // Save the user OAuth profile
-      users.saveOAuthUserProfile(req, providerUserProfile, done);
-    }));
+    // Save the user OAuth profile
+    users.saveOAuthUserProfile(req, providerUserProfile, done);
+  }));
 };
